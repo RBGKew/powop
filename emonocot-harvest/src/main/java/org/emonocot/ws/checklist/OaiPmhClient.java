c8b8d3d265326caa7a71d053791b98fb1fc61489
jk00kg <jk00kg>
2011-09-01 09:51:38 +0000

    Kew specific changes and moving changelog

--- emonocot-harvest/src/main/java/org/emonocot/ws/checklist/OaiPmhClient.java
+++ emonocot-harvest/src/main/java/org/emonocot/ws/checklist/OaiPmhClient.java
@@ -26,7 +26,6 @@ import org.joda.time.DateTime;
 import org.joda.time.DateTimeZone;
 import org.joda.time.format.DateTimeFormatter;
 import org.joda.time.format.ISODateTimeFormat;
-import org.openarchives.pmh.Metadata;
 import org.openarchives.pmh.Record;
 import org.openarchives.pmh.ResumptionToken;
 import org.slf4j.Logger;
