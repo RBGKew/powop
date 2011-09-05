2309eff3b81b79fa9db479da0061b3761893ed43
annapaola <annapaola>
2011-09-05 14:09:10 +0000

    READY_FOR_REVIEW - bug 36: Simplest possible Taxon Page http://129.67.24.160/bugzilla/show_bug.cgi?id=36

--- emonocot-portal/src/test/java/org/emonocot/portal/PortalFunctionalTest.java
+++ emonocot-portal/src/test/java/org/emonocot/portal/PortalFunctionalTest.java
@@ -1,8 +1,12 @@
 package org.emonocot.portal;
 
+import java.util.Properties;
+
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.Resource;
 
 import com.thoughtworks.selenium.DefaultSelenium;
 import com.thoughtworks.selenium.SeleneseTestCase;
@@ -13,6 +17,10 @@ import com.thoughtworks.selenium.SeleneseTestCase;
  *
  */
 public class PortalFunctionalTest extends SeleneseTestCase {
+   /**
+    *
+    */
+   private Properties properties;
     /**
      * The default port of the Selenium RC Server.
      */
@@ -22,11 +30,16 @@ public class PortalFunctionalTest extends SeleneseTestCase {
      *
      */
     @Before
-    public final void setUp() {
+    public final void setUp()  throws Exception {
+    	Resource propertiesFile
+        = new ClassPathResource("application.properties");
+        properties = new Properties();
+        properties.load(propertiesFile.getInputStream());
+        
         selenium = new DefaultSelenium("localhost",
         PortalFunctionalTest.SELENIUM_SERVER_PORT,
-        System.getProperty("selenium.browser.profile","*firefox /usr/lib/firefox-3.6/firefox"),
-        System.getProperty("selenium.target.portal","http://build.e-monocot.org/latest/portal")
+        properties.getProperty("selenium.browser.profile","*firefox /usr/lib/firefox-3.6/firefox"),
+        properties.getProperty("selenium.target.portal","http://build.e-monocot.org/latest/portal")
         );
         selenium.start();
     }
@@ -37,6 +50,18 @@ public class PortalFunctionalTest extends SeleneseTestCase {
     @Test
     public final void testSimple() {
         assertTrue(selenium.isTextPresent(""));
+    }
+    
+    /**
+     *Bug 36
+     */
+    @Test
+    public final void testItalicName() {
+    	selenium.open("taxon/urn:kew.org:wcs:taxon:2295");
+    	selenium.selectWindow("null");
+    	verifyTrue(selenium.isTextPresent("Acorus"));
+    	assertEquals("taxonName", selenium.getAttribute("css=article > div.row > div.twelvecol > header > h2 > span@class"));
+
     }
 
     /**
