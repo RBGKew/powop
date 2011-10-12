82ccd69420b275edb42a1e02d8184d828c70e4a7
jk00kg <jk00kg>
2011-10-12 17:16:04 +0000

    IN_DEVELOPMENT - bug 99: Create a provider for results of a harvesting job http://129.67.24.160/bugzilla/show_bug.cgi?id=99
    Unable to figure out why changes to driver test and schema files cause h2 db not to load

--- emonocot-persistence/src/test/java/org/emonocot/persistence/olap/DriverManagerTest.java
+++ emonocot-persistence/src/test/java/org/emonocot/persistence/olap/DriverManagerTest.java
@@ -3,24 +3,22 @@
  */
 package org.emonocot.persistence.olap;
 
-
 import static org.junit.Assert.*;
 
-import java.io.File;
-
 import javax.sql.DataSource;
-
-import org.olap4j.OlapConnection;
-
+import mondrian.olap.Axis;
 import mondrian.olap.Connection;
 import mondrian.olap.DriverManager;
+import mondrian.olap.Position;
+import mondrian.olap.Query;
+import mondrian.olap.Result;
 import mondrian.olap.Util.PropertyList;
 import mondrian.rolap.RolapConnectionProperties;
-import mondrian.spi.CatalogLocator;
-import mondrian.spi.impl.CatalogLocatorImpl;
 
+import org.emonocot.persistence.AbstractPersistenceTest;
 import org.junit.After;
 import org.junit.Before;
+import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -29,11 +27,11 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 /**
  * @author jk00kg
- *
+ * 
  */
 @RunWith(SpringJUnit4ClassRunner.class)
-@ContextConfiguration(locations={"/applicationContext-test.xml"})
-public class DriverManagerTest {
+@ContextConfiguration(locations = { "/applicationContext-test.xml" })
+public class DriverManagerTest extends AbstractPersistenceTest{
 
     /**
      * 
@@ -42,11 +40,22 @@ public class DriverManagerTest {
     /**
      * 
      */
-    private DataSource dataSource;
     private Connection conx;
+    /**
+     * 
+     */
+    private DataSource dataSource;
 
     /**
-     * @param dataSource the dataSource to set
+     * @param mondrianConnection the conx to set
+     */
+//    public void setMondrianConnection(Connection mondrianConnection) {
+//        this.conx = mondrianConnection;
+//    }
+
+    /**
+     * @param dataSource
+     *            the dataSource to set
      */
     @Autowired
     public void setDataSource(DataSource dataSource) {
@@ -58,10 +67,10 @@ public class DriverManagerTest {
      */
     @Before
     public void setUp() throws Exception {
-        //Catalog name needs to be given from root directory/webapp root
+        // Catalog name needs to be given from root directory/webapp root
         properties = new PropertyList();
-        properties.put(RolapConnectionProperties.Catalog.name(), "target/test-classes/testMondrianSchema.xml");
-        properties.put(RolapConnectionProperties.PoolNeeded.name(), "true");
+        properties.put(RolapConnectionProperties.Catalog.name(),
+                "target/test-classes/testMondrianSchema.xml");
     }
 
     /**
@@ -70,12 +79,30 @@ public class DriverManagerTest {
     @After
     public void tearDown() throws Exception {
     }
-    
+
     @Test
-    public void createMondrianConnectionTest(){
-        if(dataSource == null) fail("The flipping context hasn't loaded");
+    public void createMondrianConnectionTest() {
+        if (dataSource == null)
+            fail("The context hasn't loaded");
         conx = DriverManager.getConnection(properties, null, dataSource);
         assertNotNull(conx);
+    }
+
+    public void queryTest() {
+
+        // TODO make it work like a proper test
+        Query query = conx
+                .parseQuery("SELECT {[Measures].[number harvested]} on COLUMNS,"
+                        + " {[taxa].members} on ROWS" + " FROM harvest");
+
+        Result result = conx.execute(query);
+        assertNotNull(result);
+        Axis[] axes = result.getAxes();
+        for (int i = 0; i < axes.length; i++) {
+            for (Position pos : axes[i].getPositions()) {
+                System.out.println(pos.size());
+            }
+        }
     }
 
 }
