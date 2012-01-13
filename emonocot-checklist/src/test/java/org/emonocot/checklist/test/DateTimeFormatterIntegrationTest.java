eb2e8304fe3ad243e4a53156dcbe186574cf0921
ben <ben>
2012-01-13 09:07:24 +0000

    Improved logging for this test so we can figure out why it fails under linux

--- emonocot-checklist/src/test/java/org/emonocot/checklist/test/DateTimeFormatterIntegrationTest.java
+++ emonocot-checklist/src/test/java/org/emonocot/checklist/test/DateTimeFormatterIntegrationTest.java
@@ -9,44 +9,58 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.convert.ConversionService;
-import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
+/**
+ *
+ * @author ben
+ *
+ */
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration({ "/applicationContext-test.xml" })
 public class DateTimeFormatterIntegrationTest {
-	
-	@Autowired
-	ConversionService conversionService;
-	
-	@Test
-	public void testDateTimeFormatter() {
-		DateTimeFormatter dateTimeFormatter = ISODateTimeFormat.dateTime();
-		try{
-			dateTimeFormatter.parseDateTime("2010-06-17T10:45:45.067Z");
-		} catch(Exception e) {
-			fail("No " + e.getClass().getName() + " expected here");
-		}
-		
-	}
 
-	@Test
-	public void testConversionService() {
+    /**
+     *
+     */
+    @Autowired
+    private ConversionService conversionService;
 
-		try{
-			conversionService.convert("17/06/10 10:45", DateTime.class);
-		} catch(Exception e) {
-			for(StackTraceElement ste : e.getStackTrace()) {
-				System.err.println(ste);
-			}
-			
-			for(StackTraceElement ste : e.getCause().getStackTrace()) {
-				System.err.println(ste);
-			}
-			fail("No " + e.getClass().getName() + " expected here");
-		}
-		
-	}
-	
+    /**
+     *
+     */
+    @Test
+    public final void testDateTimeFormatter() {
+        DateTimeFormatter dateTimeFormatter = ISODateTimeFormat.dateTime();
+        try {
+            dateTimeFormatter.parseDateTime("2010-06-17T10:45:45.067Z");
+        } catch (Exception e) {
+            fail("No " + e.getClass().getName() + " expected here");
+        }
+
+    }
+
+    /**
+     *
+     */
+    @Test
+    public final void testConversionService() {
+
+        try {
+            conversionService.convert("17/06/10 10:45", DateTime.class);
+        } catch (Exception e) {
+            System.err.println(e.getMessage());
+            for (StackTraceElement ste : e.getStackTrace()) {
+                System.err.println(ste);
+            }
+            System.err.println(e.getCause().getMessage());
+            for (StackTraceElement ste : e.getCause().getStackTrace()) {
+                System.err.println(ste);
+            }
+            fail("No " + e.getClass().getName() + " expected here");
+        }
+
+    }
+
 }
