c010d4c5ff2d5f4e08b8d86758e115245cdb2734
ben <ben>
2012-02-28 15:23:12 +0000

    Minor tweaks to the taxon pages so that the subnav bar scrollspy behaviour is a bit better

--- emonocot-portal/src/main/js/bootstrap-scrollspy.js
+++ emonocot-portal/src/main/js/bootstrap-scrollspy.js
@@ -60,9 +60,11 @@
           , offsets = this.offsets
           , targets = this.targets
           , activeTarget = this.activeTarget
-          , i
+          , i;
 
+        console.log("targets[1] " + targets[1] + " offsets[1] " + offsets[1] + " scrollElementScrollTop " + this.$scrollElement.scrollTop() + " options.offset " + this.options.offset + " scrollTop" + scrollTop);
         for (i = offsets.length; i--;) {
+        	
           activeTarget != targets[i]
             && scrollTop >= offsets[i]
             && (!offsets[i + 1] || scrollTop <= offsets[i + 1])
