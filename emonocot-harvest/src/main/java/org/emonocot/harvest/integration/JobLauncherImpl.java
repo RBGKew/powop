bca1043d5b6d4f3d95d0f433dafb3838e0a6d4c7
ben <ben@zoo-benclark.zoo.ox.ac.uk>
2012-06-18 17:00:31 +0100

    IN_DEVELOPMENT - bug 202: As a emonocot developer, I want to know how to create an environment to deploy the eMonocot System http://build.e-monocot.org/bugzilla/show_bug.cgi?id=202

---
+++ emonocot-harvest/src/main/java/org/emonocot/harvest/integration/JobLauncherImpl.java
@@ -0,0 +1,114 @@
+package org.emonocot.harvest.integration;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.emonocot.api.job.JobExecutionException;
+import org.emonocot.api.job.JobExecutionInfo;
+import org.emonocot.api.job.JobInstanceInfo;
+import org.emonocot.api.job.JobLaunchRequest;
+import org.emonocot.api.job.JobLauncher;
+import org.joda.time.DateTime;
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.JobExecution;
+import org.springframework.batch.core.JobParameter;
+import org.springframework.batch.core.JobParameters;
+import org.springframework.batch.core.JobParametersInvalidException;
+import org.springframework.batch.core.configuration.JobLocator;
+import org.springframework.batch.core.launch.NoSuchJobException;
+import org.springframework.batch.core.repository.JobExecutionAlreadyRunningException;
+import org.springframework.batch.core.repository.JobInstanceAlreadyCompleteException;
+import org.springframework.batch.core.repository.JobRestartException;
+import org.springframework.beans.factory.annotation.Autowired;
+
+
+/**
+ *
+ * @author ben
+ *
+ */
+public class JobLauncherImpl implements JobLauncher {
+	
+	/**
+	 *
+	 */
+	private org.springframework.batch.core.launch.JobLauncher jobLauncher;
+	
+	/**
+	 *
+	 */
+	private JobLocator jobLocator;
+
+	/**
+	 *
+	 */
+	private String baseUrl;
+	
+	@Autowired
+	public void setJobLauncher(org.springframework.batch.core.launch.JobLauncher newJobLauncher) {
+		this.jobLauncher = newJobLauncher;
+	}
+
+	/**
+	 * @param baseUrl the baseUrl to set
+	 */
+	public void setBaseUrl(String baseUrl) {
+		this.baseUrl = baseUrl;
+	}
+
+	/**
+	 *
+	 * @param newJobLocator Set the job locator
+	 */
+	@Autowired
+	public void setJobLocator(JobLocator newJobLocator) {
+		this.jobLocator = newJobLocator;
+	}
+
+	@Override
+	public JobLaunchRequest launch(JobLaunchRequest request) {
+		Job job;
+		try {
+			job = jobLocator.getJob(request.getJob());
+			Map<String, JobParameter> jobParameterMap = new HashMap<String,	JobParameter>();
+			for(String parameterName : request.getParameters().keySet()) {
+				jobParameterMap.put(parameterName, new JobParameter(request.getParameters().get(parameterName)));
+			}
+			JobParameters jobParameters = new JobParameters(jobParameterMap);
+			try {
+				JobExecution jobExecution = jobLauncher.run(job, jobParameters);
+				JobExecutionInfo jobExecutionInfo = new JobExecutionInfo();
+				DateTime startTime = new DateTime(jobExecution.getStartTime());
+				DateTime endTime = new DateTime(jobExecution.getEndTime());
+				jobExecutionInfo.setDuration(endTime.minus(startTime.getMillis()));
+				jobExecutionInfo.setStartTime(startTime);
+				jobExecutionInfo.setExitDescription(jobExecution.getExitStatus()
+				         .getExitDescription());
+				jobExecutionInfo.setExitCode(jobExecution.getExitStatus().getExitCode());
+				jobExecutionInfo.setId(jobExecution.getId());
+				JobInstanceInfo jobInstanceInfo = new JobInstanceInfo();
+				jobInstanceInfo.setResource(baseUrl + "/jobs/"
+				+ jobExecution.getJobInstance().getJobName() + "/"
+				  + jobExecution.getJobId() + ".json");
+				jobExecutionInfo.setJobInstance(jobInstanceInfo);
+				jobExecutionInfo.setResource(baseUrl + "/jobs/executions/"
+				 + jobExecution.getJobInstance().getId() + ".json");
+				jobExecutionInfo.setStatus(jobExecution.getStatus());
+				request.setExecution(jobExecutionInfo);
+			} catch (JobExecutionAlreadyRunningException jeare) {
+				request.setException(new JobExecutionException(jeare.getLocalizedMessage()));
+			} catch (JobRestartException jre) {
+				request.setException(new JobExecutionException(jre.getLocalizedMessage()));
+			} catch (JobInstanceAlreadyCompleteException jiace) {
+				request.setException(new JobExecutionException(jiace.getLocalizedMessage()));
+			} catch (JobParametersInvalidException jpie) {
+				request.setException(new JobExecutionException(jpie.getLocalizedMessage()));
+			}
+		} catch (NoSuchJobException nsje) {
+			request.setException(new JobExecutionException(nsje.getLocalizedMessage()));			
+		}
+		
+		return request;
+	}
+
+}
---
+++ emonocot-model/src/main/java/org/emonocot/api/job/JobExecutionException.java
@@ -0,0 +1,33 @@
+package org.emonocot.api.job;
+
+public class JobExecutionException extends Throwable {
+	
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -6169550730623190349L;
+
+	/**
+	 *
+	 */
+	private String message;
+
+	public JobExecutionException(String message) {
+		this.message = message;
+	}
+
+	/**
+	 * @return the message
+	 */
+	public String getMessage() {
+		return message;
+	}
+
+	/**
+	 * @param message the message to set
+	 */
+	public void setMessage(String message) {
+		this.message = message;
+	}
+	
+}
