04e330a0cc06db1ca72dc77ef2463c1e1af6ee69
ben <ben@zoo-benclark.zoo.ox.ac.uk>
2012-07-11 17:07:08 +0100

    Selenium Grid configuration and docs

--- emonocot-portal/src/test/java/org/emonocot/portal/driver/WebDriverFacade.java
+++ emonocot-portal/src/test/java/org/emonocot/portal/driver/WebDriverFacade.java
@@ -2,16 +2,19 @@ package org.emonocot.portal.driver;
 
 import java.io.File;
 import java.io.IOException;
+import java.net.URL;
 import java.util.Properties;
 
 import javax.annotation.PreDestroy;
 
+import org.openqa.selenium.Platform;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriverService;
 import org.openqa.selenium.firefox.FirefoxBinary;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.firefox.FirefoxProfile;
 import org.openqa.selenium.firefox.internal.ProfilesIni;
+import org.openqa.selenium.ie.InternetExplorerDriverService;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.remote.RemoteWebDriver;
 import org.springframework.beans.factory.FactoryBean;
@@ -38,29 +41,66 @@ public class WebDriverFacade implements FactoryBean<WebDriver> {
                 "META-INF/spring/application.properties");
         Properties properties = new Properties();
         properties.load(propertiesFile.getInputStream());
-        String driverName = properties.getProperty("selenium.webdriver.impl",
-                "org.openqa.selenium.firefox.FirefoxDriver");
-        WebDriverType type = WebDriverType.fromString(driverName);
-
-        switch (type) {
-        case CHROME:
-        	String chromeLocation = properties.getProperty("selenium.webdriver.chrome.location");
-        	ChromeDriverService service = new ChromeDriverService.Builder()
-            .usingChromeDriverExecutable(new File(chromeLocation))
-            .usingAnyFreePort()
-            .build();
-            service.start();
-            return new RemoteWebDriver(service.getUrl(), DesiredCapabilities.chrome());
-        case FIREFOX:
-        default:
-            FirefoxBinary firefoxBinary = new FirefoxBinary();
-            String display = properties.getProperty("selenium.display.port",
-                    ":0");
-            firefoxBinary.setEnvironmentProperty("DISPLAY", display);
-            ProfilesIni allProfiles = new ProfilesIni();
-            FirefoxProfile profile = allProfiles.getProfile("default");
-            return new FirefoxDriver(firefoxBinary, profile);
-        }
+        String webdriverMode = properties.getProperty("selenium.webdriver.mode", "local");
+        String driverName = properties.getProperty("selenium.webdriver.impl", "org.openqa.selenium.firefox.FirefoxDriver");
+        WebDriverBrowserType browser = WebDriverBrowserType.fromString(driverName);
+		if (webdriverMode.equals("local")) {
+			switch (browser) {
+			case CHROME:
+				String chromeLocation = properties
+						.getProperty("selenium.webdriver.chromedriver.location");
+				ChromeDriverService chromeService = new ChromeDriverService.Builder()
+						.usingChromeDriverExecutable(new File(chromeLocation))
+						.usingAnyFreePort().build();
+				chromeService.start();
+				return new RemoteWebDriver(chromeService.getUrl(),
+						DesiredCapabilities.chrome());
+			case INTERNET_EXPLORER:
+				String 	internetExplorerLocation = properties
+						.getProperty("selenium.webdriver.ie.location");
+				InternetExplorerDriverService ieService = InternetExplorerDriverService.createDefaultService();
+				ieService.start();
+				return new RemoteWebDriver(ieService.getUrl(),
+						DesiredCapabilities.internetExplorer());
+			case FIREFOX:
+			default:
+				FirefoxBinary firefoxBinary = new FirefoxBinary();
+				String display = properties.getProperty(
+						"selenium.display.port", ":0");
+				firefoxBinary.setEnvironmentProperty("DISPLAY", display);
+				ProfilesIni allProfiles = new ProfilesIni();
+				FirefoxProfile profile = allProfiles.getProfile("default");
+				return new FirefoxDriver(firefoxBinary, profile);
+			}
+		} else {
+			
+			DesiredCapabilities capabilities = new DesiredCapabilities();			
+			switch (browser) {
+			case CHROME:
+				capabilities = DesiredCapabilities.chrome();
+				break;
+			case INTERNET_EXPLORER:
+				capabilities = DesiredCapabilities.internetExplorer();
+				break;
+			case FIREFOX:
+			default:
+				capabilities = DesiredCapabilities.firefox();
+			}
+			String platformName = properties.getProperty("selenium.webdriver.platformName", "LINUX");
+	        WebDriverPlatformType platform = WebDriverPlatformType.valueOf(platformName);
+			switch (platform) {
+			case MAC:
+				capabilities.setPlatform(Platform.MAC);
+				break;
+			case WINDOWS:
+				capabilities.setPlatform(Platform.WINDOWS);
+				break;
+			case LINUX:
+			default:
+				capabilities.setPlatform(Platform.LINUX);
+			}
+			return new RemoteWebDriver(new URL("http://build.e-monocot.org:4444/wd/hub"), capabilities);
+		}
     }
 
     /**
