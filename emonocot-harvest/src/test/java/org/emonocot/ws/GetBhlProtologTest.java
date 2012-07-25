eb7e0f2482956e4cc8a7c40908950d6c509df44f
jk00kg <jk00kg@kew.org>
2012-07-25 11:12:43 +0100

    IN_DEVELOPMENT - bug 322: As an emonocot developer, I want unit tests to pass in Kew http://build.e-monocot.org/bugzilla/show_bug.cgi?id=322
    Changed test and application context to cope with the proxy
    
    Unrelated cleaning up of the parent pom

--- emonocot-harvest/src/test/java/org/emonocot/ws/GetBhlProtologTest.java
+++ emonocot-harvest/src/test/java/org/emonocot/ws/GetBhlProtologTest.java
@@ -1,30 +1,36 @@
 package org.emonocot.ws;
 
 import org.junit.Before;
+import org.junit.Ignore;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 /**
  *
  * @author ben
  *
  */
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration("/META-INF/spring/applicationContext-test.xml")
 public class GetBhlProtologTest {
 
     /**
      *
      */
-    private BhlProtologClient client;
+    private BhlProtologClient client = new BhlProtologClient();
 
     /**
-     *
-     */
-    @Before
-    public final void setUp() {
-        client = new BhlProtologClient();
-        client.setWebServiceUrl("http://build.e-monocot.org/test/bhlResponse.json?page=");
-    }
+	 * @param client the client to set
+	 */
+    @Autowired
+	public final void setBhlProtologClient(BhlProtologClient client) {
+		this.client = client;
+	}
 
-    /**
+	/**
      *
      */
     @Test
