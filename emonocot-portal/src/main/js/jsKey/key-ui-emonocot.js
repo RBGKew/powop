2f40655e35a5bd0c76ea1d2dc596876b4f036d18
annapaola <annapaola.santarsiero@zoo.ox.ac.uk>
2012-07-26 10:13:54 +0100

    Bug 279: Some bugs fixed.

--- emonocot-portal/src/main/js/jsKey/key-ui-emonocot.js
+++ emonocot-portal/src/main/js/jsKey/key-ui-emonocot.js
@@ -119,7 +119,7 @@ function updateUI(key) {
            }
            body += "</ul>";
            $('#characterModal .modal-body').html(body);
-           
+           $('#save').unbind("click");
            $('#save').click(function() {
              var s = 1;
              var selectedValues = [];
@@ -130,144 +130,139 @@ function updateUI(key) {
                s++;
              });
              key.selectCharacter(character.id,selectedValues);
-             key.calculate();
-             var func = arguments.callee;
-             $('#save').unbind("click",func);
+             key.calculate();             
              $('#characterModal').modal('hide');
              return false;
            });
+           $("#characterModal").after("<div id='modal-gallery' class='modal modal-gallery hide fade modal-fullscreen'><div class='modal-header'><a class='close' data-dismiss='modal'>&#215;</a></div><div class='modal-body'><div class='modal-image'></div><div class='carousel-caption'><a class='modal-title'></a></div></div></div>");
+           var galleryBody = "";
+           for (var i=0; i< character.states.length; i++){
+             var state = character.states[i];
+             if(!Key.isUndefined(state.images) && state.images.length > 0) {
+                for(var j = 0; j < state.images.length; j++) {
+                   //body = "<img src='" + key.getImagePath() +  character.states[i].images[j].href + "'/>";
+                   galleryBody += "<a href='" + key.getFullsizeImagePath() +  state.images[j].href + "' rel='gallery' title='" + state.name + "'>" + state.name + "</a>";
+                }
+             }
+           }
+           
+           //var title = event.target.title;
+           $('#gallery').html(galleryBody);
 
            $("#characterModal .thumbnail").click(function(event) {
               var id = event.target.id;
-
               var temp = new Array();
               temp = id.split('-');
               var characterId = temp[0].substring(9);
+              
               var stateIndex = temp[1];
               var imageIndex = temp[2];
               var character = key.getCharacter(characterId);
-              var state = character.states[stateIndex];
               
               $('#characterModal').modal('hide');
-              var body = "";
-              for (var i=0; i< character.states.length; i++){
-            	  if(!Key.isUndefined(state.images) && state.images.length > 0) {
-            		  
-            		  for(var j = 0; j < state.images.length; j++) {
-                          //body = "<img src='" + key.getImagePath() +  character.states[i].images[j].href + "'/>";
-                          body += "<a href='" + key.getFullsizeImagePath() +  character.states[i].images[j].href + "' rel='gallery' title='" + character.states[i].name + "'>" + character.states[i].name + "</a>";
-            		  }
-            	  }
-              }
-              //var title = event.target.title;
-              $('#gallery').html(body);
               
-              $('#modal-gallery').on('hidden', function () {
-                  $('#characterModal').modal({});
-              });
               var options = {target:"#modal-gallery", slideshow:"5000", selector:"#gallery a[rel=gallery]", index: imageIndex};
               var modal = $('#modal-gallery');
-  			
+              
               options = jQuery.extend(modal.data(), options);
-  			  //modal.find('.modal-slideshow').find('i').removeClass('icon-play').addClass('icon-pause');
-  			  modal.modal(options);
-  			  
+              
+              modal.on('hidden', function() {
+            	  $('#characterModal').modal('show');
+              	 /*   modal.remove();*/
+              });
+
+              modal.modal(options);
               return false;
             });
-           $('#characterModal').on('hide', function () {
-        	   $('#save').unbind("click");
-           });
-           $('#characterModal').modal({});
-           break;
-           default:
-           // Continuous
-           body += "<div class='row'><label class='span3' for='quantitative'>Enter a value between " + character.min + " and " + character.max + "</label>"   
-           body += "<input name='quantitative' type='text' class='span3' placeholder='Type something'/><span class='help-inline'>" + character.unit + "</span></div>";
-           $('#characterModal .modal-body').html(body);
-           $('#save').click(function() {
-             var value = $('#characterModal .modal-body input').val();
-             key.selectCharacter(character.id,value);
-             key.calculate();
-             var func = arguments.callee;
-             $('#save').unbind("click",func);
-             $('#characterModal').modal('hide');
-             return false;
-           });
-           $('#characterModal').modal({});
-           break;
-         }
+           
+            $('#characterModal').modal({});
+            break;
+            default:
+            // Continuous
+            body += "<div class='row'><label class='span3' for='quantitative'>Enter a value between " + character.min + " and " + character.max + "</label>"   
+            body += "<input name='quantitative' type='text' class='span3' placeholder='Type something'/><span class='help-inline'>" + character.unit + "</span></div>";
+            $('#characterModal .modal-body').html(body);
+            $('#save').unbind("click");
+            $('#save').click(function() {
+               var value = $('#characterModal .modal-body input').val();
+               key.selectCharacter(character.id,value);
+               key.calculate();
+               $('#characterModal').modal('hide');
+               return false;
+            });
+            $('#characterModal').modal({});
+            break;
+          }
        });
       
       $(".thumbnail").click(function(event) {
-    	  
-         if(event.target.id.indexOf("character") == 0){
-          var title = event.target.title;
-           var character = key.getCharacter(event.target.id.substring(9));
-           //var body = "<img src='" + key.getImagePath() +  character.images[0].href + "'/>";
-           var body = "";
-           for(var i=0; i< character.images.length; i++){
-           body += "<a href='" + key.getFullsizeImagePath() +  character.images[i].href + "' rel='gallery' title='" + title +"'>" + title +"</a>";
-           }
-         
-           $('#gallery').html(body);
+
+          if(event.target.id.indexOf("character") == 0){
+            var title = event.target.title;
+            var character = key.getCharacter(event.target.id.substring(9));
+            //var body = "<img src='" + key.getImagePath() +  character.images[0].href + "'/>";
+            var body = "";
+            for(var i=0; i< character.images.length; i++){
+              body += "<a href='" + key.getFullsizeImagePath() +  character.images[i].href + "' rel='gallery' title='" + title +"'>" + title +"</a>";
+            }
+            
+            $('#gallery').html(body);
            //$('#modal-gallery .modal-body .carousel-caption .modal-title').html(title);
-         } else {
-           var title = event.target.title;
+          } else {
+            var title = event.target.title;
            
-           var descriptiveConcept = key.getDescriptiveConcept(event.target.id.substring(18));
-           //var body = "<img src='" + key.getImagePath() +  descriptiveConcept.images[0].href + "'/>";
-           var body = "";
-           for(var i=0; i< descriptiveConcept.images.length; i++){
-           body += "<a href='" + key.getFullsizeImagePath() +  descriptiveConcept.images[i].href + "' rel='gallery' title='" + title + "'>" + title +"</a>";
-           }
-           $('#gallery').html(body);
-           //$('#modal-gallery .modal-body .carousel-caption .modal-title').html(title);
-         }
-         $('#modal-gallery').unbind('hidden');
+            var descriptiveConcept = key.getDescriptiveConcept(event.target.id.substring(18));
+            //var body = "<img src='" + key.getImagePath() +  descriptiveConcept.images[0].href + "'/>";
+            var body = "";
+            for(var i=0; i< descriptiveConcept.images.length; i++){
+              body += "<a href='" + key.getFullsizeImagePath() +  descriptiveConcept.images[i].href + "' rel='gallery' title='" + title + "'>" + title +"</a>";
+            }
+            $('#gallery').html(body);
+            //$('#modal-gallery .modal-body .carousel-caption .modal-title').html(title);
+          }
+          $('#modal-gallery').unbind('hidden');
          
-         var options = {target:"#modal-gallery", slideshow:"5000", selector:"#gallery a[rel=gallery]", index: 0};
-         var modal = $('#modal-gallery');
-			
-         options = jQuery.extend(modal.data(), options);
-	     modal.find('.modal-slideshow').find('i').removeClass('icon-play').addClass('icon-pause');
-		 modal.modal(options);
+          var options = {target:"#modal-gallery", slideshow:"5000", selector:"#gallery a[rel=gallery]", index: 0};
+          var modal = $('#modal-gallery');
+          options = jQuery.extend(modal.data(), options);
+          modal.find('.modal-slideshow').find('i').removeClass('icon-play').addClass('icon-pause');
+          modal.modal(options);
+          return false;
+        });
 
-         return false;
-      });
-
-      var selected = "";
-      for(var i = 0; i < selectedCharacters.length; i++) {
+        var selected = "";
+        for(var i = 0; i < selectedCharacters.length; i++) {
           var character = selectedCharacters[i];
           if(!Key.isUndefined(character)) {
-    		  selected += "<li class='selectedCharacter'><h4 id='" + character.id + "'>"  + character.name + "</h4>";
-                  switch(character.type) {
-                    case Key.Categorical:
-                      var values;
-                      if($.isArray(character.selectedValues)) {
-                        values = character.selectedValues;
-                      } else {
-                        values = [];
-                        values.push(character.selectedValues);
-                      }
-                      for(var j = 0; j < values.length; j++) {
-                        if(j > 0) {
+             selected += "<li class='selectedCharacter'><h4 id='" + character.id + "'>"  + character.name + "</h4>";
+             switch(character.type) {
+                case Key.Categorical:
+                   var values;
+                   if($.isArray(character.selectedValues)) {
+                      values = character.selectedValues;
+                   } else {
+                      values = [];
+                      values.push(character.selectedValues);
+                   }
+                   for(var j = 0; j < values.length; j++) {
+                      if(j > 0) {
                           selected += ", ";
-                        }
-                        var state = character.states[values[j] - 1];
-                        selected += state.name;
-                        
                       }
+                      var state = character.states[values[j] - 1];
+                      selected += state.name;
+                      
+                   }
                     break;
                     default:
                     // categorical
                     selected += character.selectedValues + " " + character.unit;
-                    break;                  
+                    break;
                   }
                   selected += "</li>";
-    	  }
-      }      
-      $("#selectedCharacters").html("<li class='nav-header'>Features Chosen: " + selectedCharacters.length + "</li>" + selected);
-      $("#selectedCharacters li h4").click(function(event) {
+               }
+           }      
+          $("#selectedCharacters").html("<li class='nav-header'>Features Chosen: " + selectedCharacters.length + "</li>" + selected);
+          $("#selectedCharacters li h4").click(function(event) {
           key.unselectCharacter(event.target.id);
           key.calculate();
       });
