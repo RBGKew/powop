ff183b5ce391ac90d08ad7a7a5de269e67e7c58b
ben <benjamin.r.clark@gmail.com>
2012-09-12 16:53:15 +0100

    IN_DEVELOPMENT - bug 343: As a user of eMonocot, I would like to see properly the main image in the home page http://build.e-monocot.org/bugzilla/show_bug.cgi?id=343

--- emonocot-portal/src/main/java/org/emonocot/portal/view/Functions.java
+++ emonocot-portal/src/main/java/org/emonocot/portal/view/Functions.java
@@ -6,7 +6,6 @@ import java.util.Collection;
 import java.util.Collections;
 import java.util.Comparator;
 import java.util.Date;
-import java.util.Enumeration;
 import java.util.List;
 
 import javax.servlet.jsp.PageContext;
@@ -44,11 +43,6 @@ import org.joda.time.format.PeriodFormatter;
 import org.joda.time.format.PeriodFormatterBuilder;
 import org.springframework.batch.core.BatchStatus;
 import org.springframework.core.convert.support.DefaultConversionService;
-import org.springframework.expression.EvaluationContext;
-import org.springframework.expression.Expression;
-import org.springframework.expression.ExpressionParser;
-import org.springframework.expression.spel.standard.SpelExpressionParser;
-import org.springframework.expression.spel.support.StandardEvaluationContext;
 
 import com.vividsolutions.jts.geom.Coordinate;
 import com.vividsolutions.jts.geom.Geometry;
