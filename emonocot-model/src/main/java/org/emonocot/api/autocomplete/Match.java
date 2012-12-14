e850be0d4674ebf75923637035785cfc0d5bdbf4
ben <ben@zoo-benclark.zoo.ox.ac.uk>
2012-12-14 16:47:09 +0000

    Forgot to commit these files

---
+++ emonocot-model/src/main/java/org/emonocot/api/autocomplete/Match.java
@@ -0,0 +1,38 @@
+package org.emonocot.api.autocomplete;
+
+public class Match {
+	private String label;
+	
+	private String value;
+
+	/**
+	 * @return the label
+	 */
+	public String getLabel() {
+		return label;
+	}
+
+	/**
+	 * @param label the label to set
+	 */
+	public void setLabel(String label) {
+		this.label = label;
+	}
+
+	/**
+	 * @return the value
+	 */
+	public String getValue() {
+		return value;
+	}
+
+	/**
+	 * @param value the value to set
+	 */
+	public void setValue(String value) {
+		this.value = value;
+	}
+	
+	
+
+}
