be1c1e9a586ef6027c1053fd92be5d691ed86b94
jk00kg <jk00kg@kew.org>
2013-05-15 13:18:36 +0100

    IN_DEVELOPMENT - bug 458: As a Portal user, I want to download a checklist that is easy to read http://build.e-monocot.org/bugzilla/show_bug.cgi?id=458
    -Added a list of synonyms to the hierarchical report (still a fair bit
    of formatting etc. TODO)
    -Added single valued facet for family & indexing family ranked taxa to
    be included when faceting on that family

--- emonocot-model/src/main/java/org/emonocot/model/Taxon.java
+++ emonocot-model/src/main/java/org/emonocot/model/Taxon.java
@@ -934,55 +934,62 @@ public class Taxon extends SearchableObject {
 
 	@Override
 	public SolrInputDocument toSolrInputDocument() {
-		SolrInputDocument sid = super.toSolrInputDocument();
-		sid.addField("searchable.label_sort", getScientificName());
-		addField(sid,"taxon.bibliographic_citation_t", getBibliographicCitation());
-		addField(sid,"taxon.clazz_s", getClazz());
-		addField(sid,"taxon.family_ss", getFamily());
-		if(getAcceptedNameUsage() != null) {
-		    addField(sid, "taxon.family_ss", getAcceptedNameUsage().getFamily());
-		}
-		addField(sid,"taxon.genus_s", getGenus());
-		addField(sid,"taxon.infraspecific_epithet_s", getInfraspecificEpithet());
-		addField(sid,"taxon.kingdom_s", getKingdom());
-		addField(sid,"taxon.name_published_in_t", getNamePublishedInString());
-		addField(sid,"taxon.name_published_in_year_i", getNamePublishedInYear());
-		addField(sid,"taxon.nomenclatural_code_s", getNomenclaturalCode());
-		addField(sid,"taxon.nomenclatural_status_s", getNomenclaturalStatus());
-		addField(sid,"taxon.order_s", getOrder());
-		addField(sid,"taxon.phylum_s", getPhylum());
-		addField(sid,"taxon.scientific_name_t", getScientificName());
-		addField(sid,"taxon.scientific_name_authorship_t", getScientificNameAuthorship());
-		addField(sid,"taxon.source_t", getSource());
-		addField(sid,"taxon.specific_epithet_s", getSpecificEpithet());
-		addField(sid,"taxon.subfamily_s", getSubfamily());
-		addField(sid,"taxon.subgenus_s", getSubgenus());
-		addField(sid,"taxon.subtribe_s", getSubtribe());
-		addField(sid,"taxon.taxonomic_status_s", getTaxonomicStatus());
-		addField(sid,"taxon.taxon_rank_s", getTaxonRank());
-		addField(sid,"taxon.taxon_remarks_t", getTaxonRemarks());
-		addField(sid,"taxon.tribe_s", getTribe());
-		addField(sid,"taxon.verbatim_taxon_rank_s", getVerbatimTaxonRank());
-		
-		StringBuilder summary = new StringBuilder().append(getBibliographicCitation()).append(" ")
-		.append(getClazz()).append(" ").append(getFamily()).append(" ")
-		.append(getGenus()).append(" ").append(getKingdom()).append(" ")
-		.append(getNamePublishedInString()).append(" ")
-		.append(getNamePublishedInYear()).append(" ")
-		.append(getNomenclaturalStatus()).append(" ").append(getOrder()).append(" ")
-		.append(getPhylum()).append(" ").append(getScientificName()).append(" ")
-		.append(getScientificNameAuthorship()).append(" ")
-		.append(getSource()).append(" ").append(getSpecificEpithet()).append(" ")
-		.append(getSubfamily()).append(" ").append(getSubgenus()).append(" ")
-		.append(getSubtribe()).append(" ").append(getTaxonomicStatus()).append(" ")
-		.append(getTaxonRank()).append(" ").append(getTaxonRemarks()).append(" ")
-		.append(getTribe()).append(" ").append(getVerbatimTaxonRank());
-		
-		if(getDescriptions().isEmpty()) {
-			sid.addField("taxon.descriptions_not_empty_b", false);
-		} else {
-			sid.addField("taxon.descriptions_not_empty_b", true);
-		}
+        SolrInputDocument sid = super.toSolrInputDocument();
+        sid.addField("searchable.label_sort", getScientificName());
+        addField(sid,"taxon.bibliographic_citation_t", getBibliographicCitation());
+        addField(sid,"taxon.clazz_s", getClazz());
+        addField(sid,"taxon.family_s", getFamily());
+        if(Rank.FAMILY.equals(getTaxonRank())) {
+            addField(sid,"taxon.family_s", getScientificName());
+        }
+        addField(sid,"taxon.family_ss", getFamily());
+        if(getAcceptedNameUsage() != null) {
+            addField(sid, "taxon.family_ss", getAcceptedNameUsage().getFamily());
+            if(Rank.FAMILY.equals(getAcceptedNameUsage().getTaxonRank())) {
+                addField(sid,"taxon.family_s", getAcceptedNameUsage().getScientificName());
+            }
+        }
+        addField(sid,"taxon.genus_s", getGenus());
+        addField(sid,"taxon.infraspecific_epithet_s", getInfraspecificEpithet());
+        addField(sid,"taxon.kingdom_s", getKingdom());
+        addField(sid,"taxon.name_published_in_t", getNamePublishedInString());
+        addField(sid,"taxon.name_published_in_year_i", getNamePublishedInYear());
+        addField(sid,"taxon.nomenclatural_code_s", getNomenclaturalCode());
+        addField(sid,"taxon.nomenclatural_status_s", getNomenclaturalStatus());
+        addField(sid,"taxon.order_s", getOrder());
+        addField(sid,"taxon.phylum_s", getPhylum());
+        addField(sid,"taxon.scientific_name_t", getScientificName());
+        addField(sid,"taxon.scientific_name_authorship_t", getScientificNameAuthorship());
+        addField(sid,"taxon.source_t", getSource());
+        addField(sid,"taxon.specific_epithet_s", getSpecificEpithet());
+        addField(sid,"taxon.subfamily_s", getSubfamily());
+        addField(sid,"taxon.subgenus_s", getSubgenus());
+        addField(sid,"taxon.subtribe_s", getSubtribe());
+        addField(sid,"taxon.taxonomic_status_s", getTaxonomicStatus());
+        addField(sid,"taxon.taxon_rank_s", getTaxonRank());
+        addField(sid,"taxon.taxon_remarks_t", getTaxonRemarks());
+        addField(sid,"taxon.tribe_s", getTribe());
+        addField(sid,"taxon.verbatim_taxon_rank_s", getVerbatimTaxonRank());
+        
+        StringBuilder summary = new StringBuilder().append(getBibliographicCitation()).append(" ")
+        .append(getClazz()).append(" ").append(getFamily()).append(" ")
+        .append(getGenus()).append(" ").append(getKingdom()).append(" ")
+        .append(getNamePublishedInString()).append(" ")
+        .append(getNamePublishedInYear()).append(" ")
+        .append(getNomenclaturalStatus()).append(" ").append(getOrder()).append(" ")
+        .append(getPhylum()).append(" ").append(getScientificName()).append(" ")
+        .append(getScientificNameAuthorship()).append(" ")
+        .append(getSource()).append(" ").append(getSpecificEpithet()).append(" ")
+        .append(getSubfamily()).append(" ").append(getSubgenus()).append(" ")
+        .append(getSubtribe()).append(" ").append(getTaxonomicStatus()).append(" ")
+        .append(getTaxonRank()).append(" ").append(getTaxonRemarks()).append(" ")
+        .append(getTribe()).append(" ").append(getVerbatimTaxonRank());
+        
+        if(getDescriptions().isEmpty()) {
+            sid.addField("taxon.descriptions_not_empty_b", false);
+        } else {
+            sid.addField("taxon.descriptions_not_empty_b", true);
+        }
 
 		for(Description d : getDescriptions()) {
 			summary.append(" ").append(d.getDescription());
