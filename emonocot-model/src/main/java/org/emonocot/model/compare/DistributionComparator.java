f5cd16d7eff7edf4e40bf0d2f77f395cae9c1258
ben <benjamin.clark@zoo.ox.ac.uk>
2013-06-20 16:55:17 +0100

    Distribution comparator

---
+++ emonocot-model/src/main/java/org/emonocot/model/compare/DistributionComparator.java
@@ -0,0 +1,29 @@
+package org.emonocot.model.compare;
+
+import java.util.Comparator;
+
+import org.emonocot.model.Distribution;
+import org.emonocot.model.constants.Location;
+
+public class DistributionComparator  implements Comparator<Distribution> {
+	
+	Comparator<Location> locationComparator = new LocationComparator();
+
+	@Override
+	public int compare(Distribution o1, Distribution o2) {
+		if(o1.getLocation() == null) {
+			if(o2.getLocality() == null) {
+				return 0;
+			} else {
+				return -1;
+			}
+		} else {
+			if(o2.getLocality() == null) {
+				return 1;
+			} else {
+				return locationComparator.compare(o1.getLocation(), o2.getLocation()); 
+			}
+		}
+	}
+
+}
