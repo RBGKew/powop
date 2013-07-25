89fc3753366264a6e8037f7ef56d77e06298c72e
ben <benjamin.clark@zoo.ox.ac.uk>
2013-07-25 15:54:07 +0100

    Added Optimize tasklet

--- emonocot-harvest/src/main/java/org/emonocot/harvest/common/DirectoryReader.java
+++ emonocot-harvest/src/main/java/org/emonocot/harvest/common/DirectoryReader.java
@@ -7,16 +7,27 @@ import org.springframework.batch.item.database.AbstractPagingItemReader;
 
 public class DirectoryReader extends AbstractPagingItemReader<File> {
 	
+	private File[] files;
+	
 	private String directoryName;
 	
 	public void setDirectoryName(String directoryName) {
 		this.directoryName = directoryName;
 	}
+	
+	@Override
+	protected void doOpen() {
+		File directory = new File(directoryName);
+		files = directory.listFiles();
+	}
+	
+	@Override
+	protected void doClose() {
+		files = null;
+	}
 
 	@Override
-	protected void doReadPage() {
-		File directory = new File(directoryName);
-		File[] files = directory.listFiles();
+	protected void doReadPage() {		
 		int from = this.getPageSize() * this.getPage();
 		int to = from + this.getPageSize();
 		results = Arrays.asList(Arrays.copyOfRange(files, from, to));		
