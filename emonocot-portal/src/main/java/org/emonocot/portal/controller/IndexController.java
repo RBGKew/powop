6b31fabbde59e86aafd90afdf312035a430543b4
annapaola <annapaola.santarsiero@zoo.ox.ac.uk>
2013-07-29 14:49:53 +0100

    Add a statement to the front page about the content in the portal

--- emonocot-portal/src/main/java/org/emonocot/portal/controller/IndexController.java
+++ emonocot-portal/src/main/java/org/emonocot/portal/controller/IndexController.java
@@ -1,11 +1,15 @@
 package org.emonocot.portal.controller;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 import org.apache.solr.client.solrj.SolrServerException;
 import org.emonocot.api.CommentService;
+import org.emonocot.api.SearchableObjectService;
 import org.emonocot.model.Comment;
+import org.emonocot.model.SearchableObject;
 import org.emonocot.pager.Page;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -18,19 +22,31 @@ import org.springframework.web.bind.annotation.RequestMethod;
 public class IndexController {
 	
 	private CommentService commentService;
+	
+	private SearchableObjectService searchableObjectService;
 
 	@Autowired
 	public void setCommentService(CommentService commentService) {
 		this.commentService = commentService;
 	}
 	
+	@Autowired
+	public void setSearchableObjectService(SearchableObjectService searchableObjectService) {
+		this.searchableObjectService = searchableObjectService;
+	}
+
+
 	@RequestMapping(method = RequestMethod.GET,produces = "text/html")
 	public String index(Model uiModel) throws SolrServerException {
 		Map<String, String> selectedFacets = new HashMap<String, String>();		
 		selectedFacets.put("base.class_s", "org.emonocot.model.Comment");
 		selectedFacets.put("comment.status_t", "SENT");
-		Page<Comment> result = commentService.search(null, null, 5, 0, null, null, selectedFacets, "comment.created_dt_desc", "aboutData");
-		uiModel.addAttribute("comments", result);
+		Page<Comment> comments = commentService.search(null, null, 5, 0, null, null, selectedFacets, "comment.created_dt_desc", "aboutData");
+		uiModel.addAttribute("comments", comments);
+		List<String> responseFacets = new ArrayList<String>();
+		responseFacets.add("base.class_s");
+		Page<SearchableObject> stats = searchableObjectService.search("", null, 1, 0, responseFacets.toArray(new String[1]), null, null, null, null);
+		uiModel.addAttribute("stats", stats);
 		return "index";
 	}
 
