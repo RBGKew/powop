4c221b8de3c4dd630b7e34c0cd1abff53c8129dd
ben <benjamin.clark@zoo.ox.ac.uk>
2013-08-07 12:04:33 +0100

    ErrorListener and comparators

---
+++ emonocot-model/src/main/java/org/emonocot/model/compare/ReferenceComparator.java
@@ -0,0 +1,40 @@
+package org.emonocot.model.compare;
+
+import java.util.Comparator;
+
+import org.apache.commons.collections.comparators.NullComparator;
+import org.emonocot.model.Reference;
+
+public class ReferenceComparator implements Comparator<Reference> {
+	
+	private NullComparator nullSafeStringComparator = new NullComparator(
+			new Comparator<String>() {
+
+				@Override
+				public int compare(String o1, String o2) {
+					return o1.compareTo(o2);
+				}
+
+			});
+
+	@Override
+	public int compare(Reference o1, Reference o2) {
+		int compareDate = nullSafeStringComparator.compare(o1.getDate(), o2.getDate());
+    	if(compareDate == 0) {
+    		int compareAuthor = nullSafeStringComparator.compare(o1.getCreator(), o2.getCreator());
+    		if(compareAuthor == 0) {
+    			int compareTitle = nullSafeStringComparator.compare(o1.getTitle(), o2.getTitle());
+    			if(compareTitle == 0) {
+    				return nullSafeStringComparator.compare(o1.getBibliographicCitation(), o2.getBibliographicCitation());        				
+    			} else {
+    				return compareTitle;
+    			}
+    		} else {
+    			return compareAuthor;
+    		}
+    	} else {
+    		return -1 * compareDate;
+    	}
+	}
+
+}
