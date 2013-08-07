4b96d052c54326043eae275eea680edcaa6336a4
ben <benjamin.clark@zoo.ox.ac.uk>
2013-08-07 12:58:34 +0100

    Corrected BibliographyTest

--- emonocot-portal/src/test/java/org/emonocot/portal/view/BibliographyTest.java
+++ emonocot-portal/src/test/java/org/emonocot/portal/view/BibliographyTest.java
@@ -1,22 +1,23 @@
 package org.emonocot.portal.view;
 
-import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
 
-import java.util.HashMap;
 import java.util.HashSet;
-import java.util.Set;
 
+import org.emonocot.model.Description;
 import org.emonocot.model.Reference;
 import org.emonocot.model.Taxon;
-import org.emonocot.model.Description;
-import org.emonocot.model.constants.DescriptionType;
 import org.emonocot.portal.view.bibliography.Bibliography;
 import org.emonocot.portal.view.bibliography.SimpleBibliographyImpl;
 import org.junit.Before;
 import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class BibliographyTest {
+	
+	private static Logger logger = LoggerFactory.getLogger(BibliographyTest.class);
 	
 	private Bibliography bibliography;
 	
@@ -34,13 +35,13 @@ public class BibliographyTest {
 	public void setUp() {
 		Taxon taxon = new Taxon();
 		taxon.setDescriptions(new HashSet<Description>());
-		reference1 = createReference("1753", null, null, null, "1");
+		reference1 = createReference("1753", null, "Species Plantarum", null, "1");
 		taxon.getReferences().add(reference1);
 		
-		reference2 = createReference(null, "Zebras are wonderful creatures", null, null, "2");
+		reference2 = createReference(null, "Zebras are wonderful creatures", "Zebras are wonderful creatures", null, "2");
 		taxon.getReferences().add(reference2);
 		
-		reference3 = createReference(null, "A", null, null, "3");
+		reference3 = createReference(null, "Aardvarks are horrible", "Aardvarks are horrible", null, "3");
 		taxon.getReferences().add(reference3);
 		
 		duplicateReference4 = createReference("2005","The flowering plants of Mt. Popa, central Myanmar - Results of Myanmar-Japanese joint expeditions, 2000-2004","Tanaka, N., Koyama, T. & Murata, J. (2005). The flowering plants of Mt. Popa, central Myanmar - Results of Myanmar-Japanese joint expeditions, 2000-2004. Makinoa 5: 1-102.","Tanaka, N., Koyama, T. & Murata, J.","4");
@@ -56,6 +57,8 @@ public class BibliographyTest {
 	private Reference createReference(String datePublished, String title, String bibliographicCitation, String creator, String identifier) {
 		Reference reference = new Reference();
 		reference.setDate(datePublished);
+		reference.setBibliographicCitation(bibliographicCitation);
+		reference.setCreator(creator);
 		reference.setTitle(title);
 		reference.setIdentifier(identifier);
 		return reference;
@@ -69,10 +72,14 @@ public class BibliographyTest {
 	public void testBibliography() {
 		assertNotNull("Bibliography should not be null", bibliography);
 		assertEquals("Bibliography should contain four items", bibliography.getReferences().size(),4);
-		assertEquals("References with dates should come first", bibliography.getKey(reference1),"1");
+		for(Reference ref : bibliography.getReferences()) {
+			logger.info(bibliography.getKey(ref) + " " + ref.getDate() + " " +ref.getBibliographicCitation());
+		}
+		assertEquals("References with earlier dates should come first", bibliography.getKey(duplicateReference4),"1");
+		assertEquals("References with dates should before other references", bibliography.getKey(reference1),"2");
 		assertEquals("References without dates should be sorted by author, then title", bibliography.getKey(reference2),"4");
-		assertEquals("Duplicate references should return the same key", bibliography.getKey(duplicateReference4),"2");
-		assertEquals("Duplicate references should return the same key", bibliography.getKey(duplicateReference5),"2");
+		assertEquals("Duplicate references should return the same key", bibliography.getKey(duplicateReference4),"1");
+		assertEquals("Duplicate references should return the same key", bibliography.getKey(duplicateReference5),"1");
 	}
 
 }
