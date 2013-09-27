59a7ac1ece7b6913d54fed23cc48c34bdb68194a
ben <benjamin.clark@zoo.ox.ac.uk>
2013-09-27 11:00:17 +0100

    BUG #566 As a user I would like that the default of a search should be A - Z and taxa come first

--- emonocot-persistence/src/main/java/org/emonocot/persistence/dao/hibernate/SearchableDaoImpl.java
+++ emonocot-persistence/src/main/java/org/emonocot/persistence/dao/hibernate/SearchableDaoImpl.java
@@ -97,7 +97,7 @@ public abstract class SearchableDaoImpl<T extends Base> extends DaoImpl<T>
         solrQuery.set("spellcheck", "true");
         solrQuery.set("spellcheck.collate", "true");
         solrQuery.set("spellcheck.count", "1");
-        
+        solrQuery.set("bq", "base.class_s:org.emonocot.model.Taxon^2.0");
         
         // Filter the searchable objects out
         solrQuery.addFilterQuery("base.class_searchable_b:" + isSearchableObject());
