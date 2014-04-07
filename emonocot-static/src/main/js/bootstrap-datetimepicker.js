519e0d9dc575061a7ca330091656cdfbedf2acf6
Matthew Blissett <M.Blissett@kew.org>
2014-04-07 12:23:09 +0100

    Converted bootstrap-datetimepicker.js to ASCII (Changed Sébastien to Sebastien, removed BOM).

--- emonocot-static/src/main/js/bootstrap-datetimepicker.js
+++ emonocot-static/src/main/js/bootstrap-datetimepicker.js
@@ -1,9 +1,9 @@
-﻿/* =========================================================
+/* =========================================================
  * bootstrap-datetimepicker.js
  * =========================================================
  * Copyright 2012 Stefan Petre
  * Improvements by Andrew Rowls
- * Improvements by Sébastien Malot
+ * Improvements by Sebastien Malot
  * Project URL : http://www.malot.fr/bootstrap-datetimepicker
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
