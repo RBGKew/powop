53bb684391f0302f50c913f7196c0b9e994bd897
James Crowe <james.crowe@kew.org>
2015-10-26 16:42:34 +0000

    update to TaxonDonothingWriter. It still does nothing, but no longer breaks things while doing so.

--- emonocot-harvest/src/main/java/org/emonocot/job/dwc/taxon/TaxonDoNothingWriter.java
+++ emonocot-harvest/src/main/java/org/emonocot/job/dwc/taxon/TaxonDoNothingWriter.java
@@ -4,9 +4,8 @@ import java.util.List;
 
 import org.emonocot.model.Taxon;
 import org.springframework.batch.item.ItemWriter;
-import org.springframework.orm.hibernate3.support.HibernateDaoSupport;
 
-public class TaxonDoNothingWriter extends HibernateDaoSupport implements ItemWriter<Taxon>{
+public class TaxonDoNothingWriter  implements ItemWriter<Taxon>{
 
 	@Override
 	public void write(List<? extends Taxon> taxonlist) throws Exception {
