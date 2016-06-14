f592b22c3f8773074d5daab4477607154a66e830
John Iacona <J.Iacona@kew.org>
2016-06-14 11:05:02 +0100

    Update description types enumerations

--- bin/description-types.rb
+++ bin/description-types.rb
@@ -2,17 +2,19 @@
 
 filename = ARGV.shift
 
-puts "reading from #{filename}"
+original = []
+enum = []
+display = []
 File.readlines(filename).each do |line|
   line.strip!
-#  tokens = line.split(':')
-#  enumName = tokens.last
-#
-#  if %w[male female].include?(enumName)
-#    enumName += tokens[-2].capitalize
-#  end
-#
-  enumName = line.gsub(/-(.)/) {$1.capitalize}.gsub(/\s*:\s*(.)/) {$1.capitalize}
-
-  puts %|#{enumName}("#{line}"),|
+  e, d = line.split(',')
+  enum.push  e.gsub(/-(.)/) {$1.capitalize}.gsub(/\s*:\s*(.)/) {$1.capitalize}
+  display.push d
+  original.push e
 end
+
+enum.each_with_index { |e, i|  puts %|#{e}("#{original[i]}"),| }
+
+puts "\n\n-------------------------------------------------------\n\n"
+
+display.each_with_index { |d, i|  puts %|#{enum[i]}=#{d}| }
