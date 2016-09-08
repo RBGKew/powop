9b661db920a0cbbe1f3ed4ea44019f377fc4453b
John Iacona <J.Iacona@kew.org>
2016-09-08 15:04:48 +0100

    Only show first three images in search results

--- emonocot-portal/src/main/frontend/src/js/helpers/images-helper.js
+++ emonocot-portal/src/main/frontend/src/js/helpers/images-helper.js
@@ -6,4 +6,14 @@ define(['handlebars'], function(Handlebars) {
   Handlebars.registerHelper('fullsizeImage', function(image, options) {
     return new Handlebars.SafeString('<img src="' + image.url + '_fullsize.jpg" title="'+ image.caption + '" />');
   });
+
+  Handlebars.registerHelper('firstThree', function(images, options) {
+    var ret = "";
+
+    for(var i = 0, j = images.length; i < Math.min(j, 3); i++) {
+      ret = ret + options.fn(images[i]);
+    }
+
+    return ret;
+  });
 });
