c3bea3c4dc0d5efbeab99038c366f1c973f71125
John Iacona <J.Iacona@kew.org>
2016-09-08 16:54:54 +0100

    Initialize taxon module properly

--- emonocot-portal/src/main/frontend/src/js/taxon/main.js
+++ emonocot-portal/src/main/frontend/src/js/taxon/main.js
@@ -2,10 +2,16 @@ define(function(require) {
   var $ = require('jquery');
   var lightbox = require('lightbox');
 
-  $(document).ready(function() {
-    $(document).on('click', '[data-toggle="lightbox"]', function(e) {
-      e.preventDefault();
-      $(this).ekkoLightbox();
+  var initialize = function() {
+    $(document).ready(function() {
+      $(document).on('click', '[data-toggle="lightbox"]', function(e) {
+        e.preventDefault();
+        $(this).ekkoLightbox();
+      });
     });
-  });
+  }
+
+  return {
+    initialize: initialize,
+  }
 });
