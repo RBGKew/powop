10bd6161d9fa0f0c28b38be807fdbe97d7eaa6d5
John Iacona <J.Iacona@kew.org>
2017-03-28 11:27:51 +0100

    Tidy up formatting on static pages

--- emonocot-portal/src/main/java/org/emonocot/portal/controller/StaticController.java
+++ emonocot-portal/src/main/java/org/emonocot/portal/controller/StaticController.java
@@ -1,6 +1,8 @@
 package org.emonocot.portal.controller;
 
+import org.joda.time.DateTime;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
@@ -8,32 +10,34 @@ import org.springframework.web.bind.annotation.RequestMethod;
 public class StaticController {
 
 	@RequestMapping(value = "/generic-error", method = RequestMethod.GET, produces = {"text/html"})
-	public String error(){
+	public String error() {
 		return "/generic_error";
 	}
 
 	@RequestMapping(value = "/not-found-error", method = RequestMethod.GET, produces = {"text/html"})
-	public String notFoundError(){
+	public String notFoundError() {
 		return "/not_found_error";
 	}
 
 	@RequestMapping(value = "/privacy", method = RequestMethod.GET, produces = {"text/html"})
-	public String privacyPolicy(){
+	public String privacyPolicy() {
 		return "static/privacy";
 	}
 
 	@RequestMapping(value = "/terms-and-conditions", method = RequestMethod.GET, produces = {"text/html"})
-	public String termsAndConditions(){
+	public String termsAndConditions(Model model) {
+		model.addAttribute("date", new DateTime().toString("d MMMM y"));
+		model.addAttribute("year", new DateTime().getYear());
 		return "static/terms_and_conditions";
 	}
 
 	@RequestMapping(value = "/about", method = RequestMethod.GET, produces = {"text/html"})
-	public String about(){
+	public String about() {
 		return "static/about";
 	}
 
 	@RequestMapping(value = "/search-help", method = RequestMethod.GET, produces = {"text/html"})
-	public String searchHelp(){
+	public String searchHelp() {
 		return "static/search_help";
 	}
 }
