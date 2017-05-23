e689a49ce119470f9160c05dd1a7afb65ab91b45
John Iacona <J.Iacona@kew.org>
2017-05-23 16:52:49 +0100

    Enable authentication on harvester API

--- emonocot-harvest/src/main/java/org/emonocot/harvest/auth/WebSecurityConfig.java
+++ emonocot-harvest/src/main/java/org/emonocot/harvest/auth/WebSecurityConfig.java
@@ -1,10 +1,12 @@
 package org.emonocot.harvest.auth;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.*;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.*;
+import org.springframework.security.web.authentication.SimpleUrlAuthenticationFailureHandler;
 
 @Configuration
 @EnableWebSecurity
@@ -16,6 +18,12 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 	@Value("${harvester.password}")
 	private String password;
 
+	@Autowired
+	private RestAuthenticationEntryPoint entryPoint;
+
+	@Autowired
+	private RestSavedRequestAwareAuthenticationSuccessHandler successHandler;
+
 	@Override
 	public void configure(AuthenticationManagerBuilder auth) throws Exception {
 		auth.inMemoryAuthentication()
@@ -26,10 +34,25 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
-		http.authorizeRequests()
-		.antMatchers("/organisation/**", "/resource/**").authenticated()
-		.anyRequest().permitAll()
+		http
+		.csrf().disable()
+		.exceptionHandling()
+		.authenticationEntryPoint(entryPoint)
+		.and().authorizeRequests()
+		.antMatchers("/api/1/**").authenticated()
 		.and().formLogin()
-		.and().csrf().disable();
+		.successHandler(successHandler)
+		.failureHandler(new SimpleUrlAuthenticationFailureHandler())
+		.and().logout();
+	}
+
+	@Bean
+	public RestSavedRequestAwareAuthenticationSuccessHandler apiSuccessHandler() {
+		return new RestSavedRequestAwareAuthenticationSuccessHandler();
+	}
+
+	@Bean
+	public SimpleUrlAuthenticationFailureHandler apiFailuerHandler() {
+		return new SimpleUrlAuthenticationFailureHandler();
 	}
 }
