e4e304a31e2a28a8eb5b19d9c1a4ca92ec12a841
John Iacona <J.Iacona@kew.org>
2017-06-06 17:34:13 +0100

    Show running statuses as badges

--- emonocot-portal/src/main/frontend/src/js/helpers/admin-helpers.js
+++ emonocot-portal/src/main/frontend/src/js/helpers/admin-helpers.js
@@ -1,29 +1,48 @@
 define(['handlebars', 'libs/lodash'], function(Handlebars, _) {
 
-  function statusClass(status) {
-    switch(_.toLower(status)) {
+  function pickStatusFromExecution(execution) {
+    if(_.toLower(execution.status) === 'completed') {
+      return execution.exitStatus.exitCode;
+    } else {
+      return execution.status;
+    }
+  }
+
+  function pickStatus(status, exitCode) {
+    return pickStatusFromExecution({
+      status: status,
+      exitStatus: {exitCode: exitCode}
+    });
+  }
+
+  Handlebars.registerHelper('job-status', function(execution) {
+    return pickStatusFromExecution(execution);
+  });
+
+  Handlebars.registerHelper('job-status', function(status, exitCode) {
+    return pickStatus(status, exitCode);
+  });
+
+  Handlebars.registerHelper('panel-status', function(status, exitCode) {
+    switch(_.toLower(pickStatus(status, exitCode))) {
       case 'completed':
-        return 'success';
+        return 'panel-success';
       case 'starting':
       case 'started':
       case 'stopping':
       case 'stopped':
-        return 'info';
+        return 'panel-info';
       case 'failed':
       case 'abandoned':
       case 'unknown':
-        return 'danger';
+        return 'panel-danger';
     }
 
-    return 'default';
-  }
-
-  Handlebars.registerHelper('panel-status', function(status) {
-    return 'panel-' + statusClass(status);
+    return 'panel-default';
   });
 
-  Handlebars.registerHelper('table-status', function(status) {
-    switch(_.toLower(status)) {
+  Handlebars.registerHelper('table-status', function(execution) {
+    switch(_.toLower(pickStatusFromExecution(execution))) {
       case 'starting':
       case 'started':
       case 'stopping':
