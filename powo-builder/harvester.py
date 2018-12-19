9a19c4bf4ebb367bd90c38847862ceae609ae8a1
John Iacona <j.iacona@kew.org>
2018-12-19 10:15:19 +0000

    builder: fail build if any harvest jobs fail

--- powo-builder/harvester.py
+++ powo-builder/harvester.py
@@ -11,6 +11,7 @@ API_URL = os.getenv('API_URL', '/harvester/api/1')
 API_USERNAME = os.getenv('API_USERNAME', 'admin')
 API_PASSWORD = os.getenv('API_PASSWORD', 'password')
 ENV = os.getenv('ENVIRONMENT', 'test')
+STRICT_ERROR_CHECKING = os.getenv('STRICT_ERROR_CHECKING', False)
 
 def _api(method):
     return API_PREFIX + API_URL + method
@@ -54,6 +55,25 @@ def load_data():
     run_joblist("1")
     return wait_until_data_loded("1", timeout=datetime.timedelta(hours=23))
 
+# Checks for any jobs in the given joblist for ERROR exit codes.
+# If any job failed and STRICT_ERROR_CHECKING environment variable is
+# set to true, raises a RuntimeError
+def check_job_statuses(joblist_identifier):
+    r = requests.get(_api('/job/list/' + joblist_identifier))
+    r.raise_for_status()
+    json = r.json()
+    jc = [jc for jc in json.get('jobConfigurations')]
+
+    statuses = set([j.get('jobExitCode') for j in jc])
+    error_jobs = [j.get('description') for j in jc if j.get('jobExitCode') == 'ERROR']
+
+    if 'ERROR' in statuses :
+        msg = "Harvester jobs had errors: [%s]" % ', '.join(error_jobs)
+        if STRICT_ERROR_CHECKING:
+            log.critical(msg)
+            raise RuntimeError(msg)
+        log.error(msg)
+
 def wait_until_data_loded(joblist, timeout=None):
     wait = 60
     start = datetime.datetime.now()
@@ -66,5 +86,7 @@ def wait_until_data_loded(joblist, timeout=None):
             log.error("Not ready after %ds. Timed out" % delta.seconds)
             return False
 
-    log.warn("Job list %s complete" % joblist)
+    check_job_statuses(joblist)
+
+    log.info("Job list [%s] complete" % joblist)
     return True
