666f6943ff252a5e0688a4f39d60e1a52ff7ba33
Malcolm Stone <m.stone@kew.org>
2020-11-19 11:12:23 +0000

    update name helper tests to match new output

--- powo-portal/src/test/java/org/powo/portal/view/helpers/NameHelperTest.java
+++ powo-portal/src/test/java/org/powo/portal/view/helpers/NameHelperTest.java
@@ -6,12 +6,10 @@ import org.powo.model.Taxon;
 import org.gbif.ecat.voc.Rank;
 import org.junit.Before;
 import org.junit.Test;
-import org.junit.Ignore;
 import org.powo.portal.view.helpers.NameHelper;
 
 import com.github.jknack.handlebars.Handlebars;
 
-@Ignore("Needs updating")
 public class NameHelperTest extends AbstractHelperTest {
 
 	private Taxon taxon;
@@ -34,12 +32,12 @@ public class NameHelperTest extends AbstractHelperTest {
 
 	@Test
 	public void testNameAndAuthor() throws IOException {
-		shouldCompileTo("{{nameAndAuthor this}}", taxon, "<em>Aralidiaceae</em> Philipson & B.C.Stone");
+		shouldCompileTo("{{nameAndAuthor this}}", taxon, "<em lang='la'>Aralidiaceae</em> Philipson & B.C.Stone");
 	}
 
 	@Test
 	public void testLink() throws IOException {
-		shouldCompileTo("{{taxonLink this}}", taxon, "<a href=\"/taxon/urn:lsid:ipni.org:names:1-1\"><em>Aralidiaceae</em> Philipson & B.C.Stone</a>");
+		shouldCompileTo("{{taxonLink this}}", taxon, "<a href=\"/taxon/urn:lsid:ipni.org:names:1-1\"><em lang='la'>Aralidiaceae</em> Philipson & B.C.Stone</a>");
 		shouldCompileTo("{{taxonLink this \"Blarg\"}}", taxon, "<a href=\"/taxon/urn:lsid:ipni.org:names:1-1\">Blarg</a>");
 	}
 
@@ -54,8 +52,8 @@ public class NameHelperTest extends AbstractHelperTest {
 		child.setTaxonRank(Rank.GENUS);
 
 		shouldCompileTo("{{classification this}}", child,
-				"<ul><li>Family: <a href=\"/taxon/urn:lsid:ipni.org:names:1-1\"><em>Aralidiaceae</em> Philipson & B.C.Stone</a>"
-				+ "<ul><li><h1 class=\"c-summary__heading\"><em>aaa</em> <small>L.</small></h1></li>"
-				+ "<ul></li></ul>");
+				"<ul><li>Family: <a href=\"/taxon/urn:lsid:ipni.org:names:1-1\"><em lang='la'>Aralidiaceae</em> Philipson & B.C.Stone</a>"
+				+ "<ul><li><h1 class=\"c-summary__heading\"><em lang='la'>aaa</em> <small>L.</small></h1></li>"
+				+ "</ul></li></ul>");
 	}
 }
