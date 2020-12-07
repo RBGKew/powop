b55f5934fdd5801637c146451d9c4e8bd0660a1e
David Bishop <d.bishop@kew.org>
2020-12-07 16:28:42 +0000

    added tests for name and author

--- powo-portal/src/main/frontend/src/js/helpers/taxon-helper.test.js
+++ powo-portal/src/main/frontend/src/js/helpers/taxon-helper.test.js
@@ -2,22 +2,29 @@ const Handlebars = require("handlebars");
 require("./taxon-helper");
 
 describe("nameAndAuthor", () => {
-  test.todo("handles simple name");
+  test("handles simple name", () => {
+    const taxon = {
+      name: "Bromus alopecuros",
+    };
+    const template = Handlebars.compile("{{ nameAndAuthor taxon }}");
+    expect(template({ taxon })).toEqual(
+      '<em lang="la">Bromus alopecuros</em>'
+    );
+  });
 
-  test.skip("handles name with single rank", () => {
+  test("handles name with single rank", () => {
     const taxon = {
       name: "Bromus alopecuros subsp. caroli-henrici",
     };
     const template = Handlebars.compile("{{ nameAndAuthor taxon }}");
-    console.log(template({ taxon }));
     expect(template({ taxon })).toEqual(
       '<em lang="la">Bromus alopecuros</em> subsp. <em lang="la">caroli-henrici</em>'
     );
   });
 
-  test.skip("handles name with multiple ranks", () => {
+  test("handles name with multiple ranks", () => {
     const taxon = {
-      name: "Bromus hordeaceus subsp. bicuspis var. leiostachys",
+      name: "Bromus hordeaceus subsp. bicuspis var. leiostachys"
     };
     const template = Handlebars.compile("{{ nameAndAuthor taxon }}");
     expect(template({ taxon })).toEqual(
@@ -25,30 +32,37 @@ describe("nameAndAuthor", () => {
     );
   });
 
-  test.todo("handles author");
+  test("handles author", () => {
+    const taxon = {
+      name: "Bromus hordeaceus subsp. bicuspis var. leiostachys",
+      author: "Philipson & B.C.Stone"
+    };
+    const template = Handlebars.compile("{{ nameAndAuthor taxon }}");
+    expect(template({ taxon })).toEqual(
+      '<em lang="la">Bromus hordeaceus </em> subsp. <em lang="la">bicuspis </em> var. <em lang="la">leiostachys</em> <cite>Philipson & B.C.Stone</cite>'
+    );
+  });
 });
 
 describe("taxonLink", () => {
-  test.skip("handles simple name", () => {
+  test("handles simple name", () => {
     const taxon = {
       name: "Bromus alopecuros subsp. caroli-henrici",
       url: "urn:lsid:ipni.org:names:1-1",
     };
     const template = Handlebars.compile("{{ taxonLink taxon }}");
-    console.log(template({ taxon }));
     expect(template({ taxon })).toEqual(
       '<a href="urn:lsid:ipni.org:names:1-1"><em lang="la">Bromus alopecuros</em> subsp. <em lang="la">caroli-henrici</em>'
     );
   });
 
-  test.skip("handles author", () => {
+  test("handles author", () => {
     const taxon = {
       name: "Bromus alopecuros subsp. caroli-henrici",
       url: "urn:lsid:ipni.org:names:1-1",
       author: "Philipson & B.C.Stone"
     };
     const template = Handlebars.compile("{{ taxonLink taxon }}");
-    console.log(template({ taxon }));
     expect(template({ taxon })).toEqual(
       '<a href="urn:lsid:ipni.org:names:1-1"><em lang="la">Bromus alopecuros</em> subsp. <em lang="la">caroli-henrici</em> <cite>Philipson & B.C.Stone</cite></a>'
     );
@@ -62,7 +76,6 @@ describe("color-theme", () => {
       rank: "subspecies",
     };
     const template = Handlebars.compile("{{ color-theme taxon }}");
-    console.log(template({ taxon }));
     expect(template({ taxon })).toEqual(
       's-theme-Infraspecific'
     );
@@ -74,7 +87,6 @@ describe("color-theme", () => {
       rank: "family",
     };
     const template = Handlebars.compile("{{ color-theme taxon }}");
-    console.log(template({ taxon }));
     expect(template({ taxon })).toEqual(
       's-theme-family'
     );
@@ -86,7 +98,6 @@ describe("color-theme", () => {
       rank: "subspecies",
     };
     const template = Handlebars.compile("{{ color-theme taxon }}");
-    console.log(template({ taxon }));
     expect(template({ taxon })).toEqual(
       's-theme-Synonym'
     );
