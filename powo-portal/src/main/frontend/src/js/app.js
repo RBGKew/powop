22b5b7c84f03da5963bd74ae5463e94be4ae056c
David Bishop <d.bishop@kew.org>
2021-05-07 17:18:48 +0100

    added burger menu

--- powo-portal/src/main/frontend/src/js/app.js
+++ powo-portal/src/main/frontend/src/js/app.js
@@ -4,3 +4,20 @@ require([
 ]);
 
 console.log("If you want to use POWO data programatically, please check out our python client https://github.com/RBGKew/pykew");
+
+burgerIcon = false
+$(document).ready(function() {
+  $('.toggle-nav').click(function(e) {
+    if (burgerIcon) {
+      $(this).find("use").attr("xlink:href", "#burgericon");
+      burgerIcon = false;
+    } else {
+      $(this).find("use").attr("xlink:href", "#closeicon");
+      burgerIcon = true;
+    }
+    $(this).toggleClass('active');
+    $('.top-right-nav ul').toggleClass('active');
+
+    e.preventDefault();
+  });
+});
