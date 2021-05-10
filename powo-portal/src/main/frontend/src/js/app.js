79a153323e42f1a8e4c3f75203238d8756acc7c9
David Bishop <d.bishop@kew.org>
2021-05-10 12:21:51 +0100

    added child menu, refactored js

--- powo-portal/src/main/frontend/src/js/app.js
+++ powo-portal/src/main/frontend/src/js/app.js
@@ -1,23 +1,7 @@
 require([
   'search',
-  'taxon'
+  'taxon',
+  'nav'
 ]);
 
 console.log("If you want to use POWO data programatically, please check out our python client https://github.com/RBGKew/pykew");
-
-burgerIcon = false
-$(document).ready(function() {
-  $('.toggle-nav').click(function(e) {
-    if (burgerIcon) {
-      $(this).find("use").attr("xlink:href", "#burgericon");
-      burgerIcon = false;
-    } else {
-      $(this).find("use").attr("xlink:href", "#closeicon");
-      burgerIcon = true;
-    }
-    $(this).toggleClass('active');
-    $('.top-right-nav ul').toggleClass('active');
-
-    e.preventDefault();
-  });
-});
---
+++ powo-portal/src/main/frontend/src/js/nav/main.js
@@ -0,0 +1,27 @@
+define(function(require) {
+  var $ = require('jquery');
+  var initialize = function() {
+    burgerIcon = false
+    $(document).ready(function() {
+      $('.toggle-nav').click(function(e) {
+        if (burgerIcon) {
+          $(this).find("use").attr("xlink:href", "#burgericon");
+          burgerIcon = false;
+        } else {
+          $(this).find("use").attr("xlink:href", "#closeicon");
+          burgerIcon = true;
+        }
+        $(this).toggleClass('active');
+        $('.top-right-nav ul').toggleClass('active');
+
+        e.preventDefault();
+      });
+      $('.about-toggle').click(function(e) {
+        $('.children').toggleClass('open');
+      });
+    });
+  }
+  return {
+    initialize: initialize
+  };
+}
