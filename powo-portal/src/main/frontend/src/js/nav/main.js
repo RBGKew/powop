84c1544430cefda9a71dabe449da3ff9d75ddc2d
David Bishop <d.bishop@kew.org>
2021-05-12 10:45:13 +0100

    added js for active link on nav

--- powo-portal/src/main/frontend/src/js/nav/main.js
+++ powo-portal/src/main/frontend/src/js/nav/main.js
@@ -1,5 +1,13 @@
 define(function(require) {
   var $ = require('jquery');
+  var current = location.pathname;
+  console.log(current)
+  $('.top-right-nav li a').each(function(){
+      var $this = $(this);
+      if($this.attr('href').indexOf(current) !== -1){
+          $this.addClass('current');
+      }
+  })
   burgerIcon = false
   $(document).ready(function() {
     $('.toggle-nav').click(function(e) {
