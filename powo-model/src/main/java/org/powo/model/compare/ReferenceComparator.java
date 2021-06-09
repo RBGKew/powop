7c74269ab6f1d2166ec3dc94e89920673d5ff1c0
Joaquim de Souza <j.dsouza@kew.org>
2021-06-09 15:44:29 +0100

    Order bibliography alphabetically by default

--- powo-model/src/main/java/org/powo/model/compare/ReferenceComparator.java
+++ powo-model/src/main/java/org/powo/model/compare/ReferenceComparator.java
@@ -39,21 +39,11 @@ public class ReferenceComparator implements Comparator<Reference> {
 
 	@Override
 	public int compare(Reference o1, Reference o2) {
-		int compareDate = nullSafeStringComparator.compare(o1.getDate(), o2.getDate());
-		if(compareDate == 0) {
-			int compareAuthor = nullSafeStringComparator.compare(o1.getCreator(), o2.getCreator());
-			if(compareAuthor == 0) {
-				int compareTitle = nullSafeStringComparator.compare(o1.getTitle(), o2.getTitle());
-				if(compareTitle == 0) {
-					return nullSafeStringComparator.compare(o1.getBibliographicCitation(), o2.getBibliographicCitation());
-				} else {
-					return compareTitle;
-				}
-			} else {
-				return compareAuthor;
-			}
+		int compareCitation = nullSafeStringComparator.compare(o1.getBibliographicCitation(), o2.getBibliographicCitation());
+		if (compareCitation == 0) {
+			return nullSafeStringComparator.compare(o1.getDate(), o2.getDate());
 		} else {
-			return -1 * compareDate;
+			return compareCitation;
 		}
 	}
 
