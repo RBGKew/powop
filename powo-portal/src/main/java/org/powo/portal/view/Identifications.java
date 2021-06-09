918ff16e5caefa24a73395efd642a4c0778ded66
Joaquim de Souza <j.dsouza@kew.org>
2021-06-09 09:55:07 +0100

    Refactor

--- powo-portal/src/main/java/org/powo/portal/view/Identifications.java
+++ powo-portal/src/main/java/org/powo/portal/view/Identifications.java
@@ -72,15 +72,7 @@ public class Identifications {
 				id.date = identification.getDateIdentified().toString(DateTimeFormat.mediumDate());
 			}
 
-			if (id.url != null) {
-				if (id.url.lastIndexOf("imi=") != -1) {
-					// IMI barcode
-					id.barcode = "IMI " + id.url.substring(id.url.lastIndexOf("imi=") + 1);
-				} else if (id.url.lastIndexOf('/') != -1) {
-					// Herbcat and Herbtrack barcodes
-					id.barcode = id.url.substring(id.url.lastIndexOf('/') + 1);
-				}
-			}
+			id.barcode = this.extractBarcode(id.url);
 
 			if (!taxon.equals(identification.getTaxon())) {
 				id.identifiedAs = helper.taxonLinkWithoutAuthor(identification.getTaxon(), null).toString();
@@ -89,5 +81,20 @@ public class Identifications {
 			log.debug("Adding {} from {}", id.barcode, identification.getTaxon().getIdentifier());
 			identifications.add(id);
 		}
+	}
+
+	private String extractBarcode(String identificationUrl) {
+		if (identificationUrl == null) {
+			return null;
+		}
+		if (identificationUrl.lastIndexOf("imi=") != -1) {
+			// IMI barcode
+			return "IMI " + identificationUrl.substring(identificationUrl.lastIndexOf("imi=") + 4);
+		}
+		if (identificationUrl.lastIndexOf('/') != -1) {
+			// Herbcat and Herbtrack barcodes
+			return identificationUrl.substring(identificationUrl.lastIndexOf('/') + 1);
+		}
+		return null;
 	}
 }
