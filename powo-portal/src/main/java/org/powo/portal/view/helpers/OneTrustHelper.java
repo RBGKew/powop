8a5e60c143b4f2294051ad451459cbd96a682dd5
David Bishop <d.bishop@kew.org>
2021-07-09 14:14:37 +0100

    changed one trust helper if staments to .equal and used Autowired instead of ModelAttribute

--- powo-portal/src/main/java/org/powo/portal/view/helpers/OneTrustHelper.java
+++ powo-portal/src/main/java/org/powo/portal/view/helpers/OneTrustHelper.java
@@ -4,23 +4,26 @@ import org.springframework.beans.factory.annotation.Value;
 import org.powo.site.Site;
 import com.github.jknack.handlebars.Handlebars;
 import com.google.common.base.Strings;
-import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 
 public class OneTrustHelper {
 
 	@Value("${environment.type:#{null}}")
 	String type;
+
+  @Autowired
+  @Qualifier("currentSite")
   Site site;
 
-  @ModelAttribute("oneTrustID")
   protected String oneTrustID() {
     return site.oneTrustID();
   }
 
 	public CharSequence oneTrustScript() {
-		if(type == "prod") {
+		if(type.equals("prod")) {
 			return "";
-		} else if(type == "uat") {
+		} else if(type.equals("uat")) {
 			return new Handlebars.SafeString(
 					"<script src='https://cdn.cookielaw.org/scripttemplates/otSDKStub.js'  type='text/javascript' charset='UTF-8' data-domain-script='" + oneTrustID() + "-test' ></script>" +
 					"<script type='text/javascript'>" +
