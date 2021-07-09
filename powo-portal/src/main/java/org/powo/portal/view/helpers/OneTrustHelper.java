e598e02602238e7618d43de9b8d0fe37db5facd9
David Bishop <d.bishop@kew.org>
2021-07-09 16:36:21 +0100

    added one trust helper to config

--- powo-portal/src/main/java/org/powo/portal/view/helpers/OneTrustHelper.java
+++ powo-portal/src/main/java/org/powo/portal/view/helpers/OneTrustHelper.java
@@ -3,7 +3,6 @@ package org.powo.portal.view.helpers;
 import org.springframework.beans.factory.annotation.Value;
 import org.powo.site.Site;
 import com.github.jknack.handlebars.Handlebars;
-import com.google.common.base.Strings;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 
