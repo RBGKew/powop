db8276e665fe9fd926f181d6ed466ee2c003e8e8
David Bishop <d.bishop@kew.org>
2021-07-13 12:49:19 +0100

    made one trust helper all tabbed

--- powo-portal/src/main/java/org/powo/portal/view/helpers/OneTrustHelper.java
+++ powo-portal/src/main/java/org/powo/portal/view/helpers/OneTrustHelper.java
@@ -8,36 +8,35 @@ import org.springframework.beans.factory.annotation.Qualifier;
 import com.google.common.base.Strings;
 
 public class OneTrustHelper {
-  
-  @Value("${environment.type:#{null}}")
+
+	@Value("${environment.type:#{null}}")
 	String type;
 
-  @Autowired
-  @Qualifier("currentSite")
-  Site site;
+	@Autowired
+	@Qualifier("currentSite")
+	Site site;
 
-  String typeProd = "prod";
-  String typeUAT = "uat";
+	String typeProd = "prod";
+	String typeUAT = "uat";
 
-  protected String oneTrustID() {
-    return site.oneTrustID();
-  }
+	protected String oneTrustID() {
+		return site.oneTrustID();
+	}
 
 	public CharSequence oneTrustScript() {
-    if(Strings.isNullOrEmpty(type)) {
+		if(Strings.isNullOrEmpty(type)) {
 			return "";
 		}
 		else if(type.equals(typeProd)) {
 			return "";
 		} else if(type.equals(typeUAT)) {
 			return new Handlebars.SafeString(
-					"<script src='https://cdn.cookielaw.org/scripttemplates/otSDKStub.js'  type='text/javascript' charset='UTF-8' data-domain-script='" + oneTrustID() + "-test' ></script>" +
-					"<script type='text/javascript'>" +
-          "function OptanonWrapper() { }" +
-          "</script>");
+				"<script src='https://cdn.cookielaw.org/scripttemplates/otSDKStub.js'  type='text/javascript' charset='UTF-8' data-domain-script='" + oneTrustID() + "-test' ></script>" +
+				"<script type='text/javascript'>" +
+				"function OptanonWrapper() { }" +
+				"</script>");
+		} else {
+			return "";
 		}
-    else {
-      return "";
-    }
 	}
 }
