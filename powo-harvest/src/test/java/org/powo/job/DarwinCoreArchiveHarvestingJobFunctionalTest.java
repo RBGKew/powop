2cf880754224afea3f2f93151a0363ac83b8ade5
Malcolm Stone <m.stone@kew.org>
2021-10-07 17:13:36 +0100

    add basic working import names job test

---
+++ powo-harvest/src/test/java/org/powo/job/DarwinCoreArchiveHarvestingJobFunctionalTest.java
@@ -0,0 +1,63 @@
+package org.powo.job;
+
+import java.util.Map;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.powo.model.constants.ResourceType;
+import org.powo.model.registry.Organisation;
+import org.powo.model.registry.Resource;
+import org.powo.persistence.dao.OrganisationDao;
+import org.powo.persistence.dao.ResourceDao;
+import org.springframework.batch.core.JobParametersBuilder;
+import org.springframework.batch.test.JobLauncherTestUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration({ 
+  "/META-INF/spring/applicationContext.xml",
+  "/META-INF/spring/applicationContext-batch.xml",
+  "/META-INF/spring/applicationContext-test.xml",
+  "/META-INF/spring/batch/jobs/darwinCoreArchiveHarvesting.xml" 
+})
+public class DarwinCoreArchiveHarvestingJobFunctionalTest {
+  @Autowired
+  private JobLauncherTestUtils jobLauncher;
+
+  @Autowired
+  private OrganisationDao organisations;
+
+  @Autowired
+  private ResourceDao resources;
+
+  @Test
+  public void importNames() throws Exception {
+    var org = new Organisation();
+		org.setIdentifier("Kew-Names-and-Taxonomic-Backbone");
+		org.setAbbreviation("Kew-Names-and-Taxonomic-Backbone");
+		org.setTitle("Kew Backbone");
+    org = organisations.save(org);
+
+    var resource = new Resource();
+    resource.setOrganisation(org);
+    resource.setUri("http://storage.googleapis.com/powop-content/backbone/if_species_name.zip");
+    resource.setTitle("Index Fungorum");
+    resource.setIdentifier("IF-Species-Names");
+    resource.setResourceType(ResourceType.DwC_Archive);
+    resource = resources.save(resource);
+    
+    var params = new JobParametersBuilder()
+      .addJobParameters(jobLauncher.getUniqueJobParameters())
+      .addString("authority.name", org.getIdentifier())
+      .addString("authority.uri", resource.getUri())
+      .addString("resource.identifier", resource.getIdentifier())
+      .addString("skip.indexing", "true")
+      .addString("taxon.processing.mode", "IMPORT_NAMES")
+      .toJobParameters();
+    jobLauncher.launchJob(params);
+  }
+}
