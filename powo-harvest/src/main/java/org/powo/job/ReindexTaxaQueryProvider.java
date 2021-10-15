206587712ff10437d7c7f19c9cc1765656b87829
Malcolm Stone <m.stone@kew.org>
2021-10-15 15:34:47 +0100

    add comment for setting read only session logic

--- powo-harvest/src/main/java/org/powo/job/ReindexTaxaQueryProvider.java
+++ powo-harvest/src/main/java/org/powo/job/ReindexTaxaQueryProvider.java
@@ -1,15 +1,9 @@
 package org.powo.job;
 
 import javax.persistence.EntityManager;
-import javax.persistence.EntityManagerFactory;
 import javax.persistence.Query;
 
 import org.hibernate.Session;
-import org.hibernate.annotations.QueryHints;
-import org.hibernate.engine.spi.EntityEntry;
-import org.hibernate.engine.spi.PersistenceContext;
-import org.hibernate.engine.spi.SharedSessionContractImplementor;
-import org.powo.model.Taxon;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.batch.item.database.orm.JpaQueryProvider;
@@ -22,35 +16,17 @@ public class ReindexTaxaQueryProvider implements JpaQueryProvider {
 
   @Override
   public Query createQuery() {
-    
-    // var taxon = entityManager.createQuery("from Taxon t where t.id = 23", Taxon.class)
-    //   .setHint(QueryHints.READ_ONLY, true)
-    //   .getSingleResult();
-
-    // var taxonState = getPersistenceContextEntry(taxon).getLoadedState();
-    // log.info("taxon loaded state {} {}", taxonState, taxonState == null);
-    
-    // var description = taxon.getDescriptions().iterator().next();
-    // var descriptionState = getPersistenceContextEntry(description).getLoadedState();
-    // log.info("description loaded state {} {}", descriptionState, descriptionState == null);
-
-    // throw new RuntimeException();
-
     return entityManager.createQuery("from Taxon t join fetch t.authority left join fetch t.acceptedNameUsage");
-      // .setHint(QueryHints.READ_ONLY, true);
   }
 
   @Override
   public void setEntityManager(EntityManager entityManager) {
     this.entityManager = entityManager;
+
+    // We set the default behaviour to be read only here because setting it as query hint only applies that read only
+    // behaviour to entities loaded in the query. Because there are lots of entities that are loaded lazily as part of
+    // reindexing we want all entities to be read as read only.
     var session = entityManager.unwrap(Session.class);
     session.setDefaultReadOnly(true);
-  }
-
-  private EntityEntry getPersistenceContextEntry(Object object) {
-    var session = entityManager.unwrap(SharedSessionContractImplementor.class);
-    var persistenceContext = session.getPersistenceContext();
-
-    return persistenceContext.getEntry(object);
   }
 }
