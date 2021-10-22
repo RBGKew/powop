a28988751599dabc66c3d9b3f4aa54cdca18bb0d
Malcolm Stone <m.stone@kew.org>
2021-10-22 12:01:57 +0100

    remove processor logging

--- powo-harvest/src/main/java/org/powo/job/dwc/read/DarwinCoreProcessor.java
+++ powo-harvest/src/main/java/org/powo/job/dwc/read/DarwinCoreProcessor.java
@@ -181,7 +181,6 @@ ItemProcessor<T, T>, ChunkListener, ItemWriteListener<T> {
 
 	@Override
 	public void afterWrite(List<? extends T> items) {
-		log.info("after write - {} annotations", chunkAnnotations.size());
 		chunkAnnotations.forEach(a -> annotationService.save(a));
 		chunkAnnotations = new ArrayList<>();
 	}
