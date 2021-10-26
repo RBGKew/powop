03756bea6159a9edebad184df24c5b3fc5831f22
Malcolm Stone <m.stone@kew.org>
2021-10-26 11:35:59 +0100

    use TaxonPersistedService for more efficient batching of updates
    using TaxonService which has FlushMode.AUTO policy means it executes the update immediately (in order to avoid stale queries). This isn't necessary in our case since we are always overwriting the existing values

--- powo-harvest/src/main/java/org/powo/job/dwc/taxon/LinkingProcessor.java
+++ powo-harvest/src/main/java/org/powo/job/dwc/taxon/LinkingProcessor.java
@@ -1,4 +1,3 @@
-package org.powo.job.dwc.taxon;
 /*
  * This is eMonocot, a global online biodiversity information resource.
  *
@@ -15,8 +14,10 @@ package org.powo.job.dwc.taxon;
  * The complete text of the GNU Affero General Public License is in the source repository as the file
  * ‘COPYING’.  It is also available from <http://www.gnu.org/licenses/>.
  */
+package org.powo.job.dwc.taxon;
 
 import org.powo.api.TaxonService;
+import org.powo.harvest.service.TaxonPersistedService;
 import org.powo.job.dwc.exception.CannotFindRecordException;
 import org.powo.job.dwc.exception.NoIdentifierException;
 import org.powo.job.dwc.read.DarwinCoreProcessor;
@@ -31,12 +32,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 public class LinkingProcessor extends DarwinCoreProcessor<Taxon> {
 	private Logger logger = LoggerFactory.getLogger(CheckingProcessor.class);
 
-	private TaxonService taxonService;
-
+	/**
+	 * We use a {@link TaxonPersistedService} here rather than {@link TaxonService} so that changes to
+	 * the taxon are not immediately flushed. Hibernate presumably does this to avoid consistency issues
+	 * but since we just overwrite the persisted taxon and don't read any details from it 
+	 */
 	@Autowired
-	public void setTaxonService(TaxonService taxonService) {
-		this.taxonService = taxonService;
-	}
+	private TaxonPersistedService taxonService;
 
 	@Override
 	public Taxon doProcess(Taxon taxon) throws Exception {
