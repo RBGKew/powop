a66b4f03a063b830210159ef1ed5e4804e1dfa84
Malcolm Stone <m.stone@kew.org>
2021-11-17 12:57:00 +0000

    update ImageHelperTest to match current functionality

--- powo-portal/src/test/java/org/powo/portal/view/helpers/ImageHelperTest.java
+++ powo-portal/src/test/java/org/powo/portal/view/helpers/ImageHelperTest.java
@@ -9,7 +9,6 @@ import org.powo.model.Taxon;
 import org.powo.model.helpers.CDNImageHelper;
 import org.junit.Before;
 import org.junit.Test;
-import org.powo.portal.view.helpers.ImageHelper;
 
 import com.github.jknack.handlebars.Handlebars;
 
@@ -52,78 +51,14 @@ public class ImageHelperTest extends AbstractHelperTest {
 	}
 
 	@Test
-	public void basicThumbnailImage() throws IOException {
-		var result = renderTemplate("{{thumbnailImage image taxon lightbox=false}}", context);
-		assertEquals("<img src=\"http://assets.blah.com/cool-img_thumbnail.jpg\" alt=\"Cool brah\"/>", result);
+	public void testImageToFullsizeUrl() throws IOException {
+		var result = renderTemplate("{{imageToThumbnailUrl image}}", context);
+		assertEquals("http://assets.blah.com/cool-img_thumbnail.jpg", result);
 	}
 
 	@Test
-	public void thumbnailWithLightbox() throws IOException {
-		var result = renderTemplate("{{thumbnailImage image taxon}}", context);
-		assertEquals(
-				"<a href=\"http://assets.blah.com/cool-img_fullsize.jpg\" title=\"Cool brah\">"
-						+ "<img src=\"http://assets.blah.com/cool-img_thumbnail.jpg\" alt=\"Cool brah\"/></a>",
-				result);
-	}
-
-	@Test
-	public void thumbnailInFigureWithLightbox() throws IOException {
-		var result = renderTemplate("{{thumbnailImage image taxon figure-class=\"woo\"}}", context);
-
-		assertEquals(
-				"<a href=\"http://assets.blah.com/cool-img_fullsize.jpg\" title=\"Cool brah\">"
-						+ "<figure class=\"woo\"><img src=\"http://assets.blah.com/cool-img_thumbnail.jpg\" alt=\"Cool brah\"/></figure></a>",
-				result);
-	}
-
-	@Test
-	public void digifoliaThumbnail() throws IOException {
-		Image image = new Image();
-		image.setIdentifier("urn:kew.org:dam:654");
-		context.setImage(image);
-
-		var result = renderTemplate("{{thumbnailImage image taxon}}", context);
-
-		assertEquals("<a href=\"https://cdn.com/13a994141177e43d57feb31d29f1e9b7.jpg\" title=\"\">"
-				+ "<img src=\"https://cdn.com/936cd5acff71ed403b5b6e1b0fa0b127.jpg\" alt=\"\"/></a>", result);
-	}
-
-	@Test
-	public void imageOnDifferentTaxonPage() throws IOException {
-		Taxon taxon = new Taxon();
-		taxon.setIdentifier("456");
-		taxon.setScientificName("Poa annua");
-		context.getImage().setTaxon(taxon);
-
-		var result = renderTemplate("{{thumbnailImage image taxon}}", context);
-
-		assertEquals(
-				"<a href=\"http://assets.blah.com/cool-img_fullsize.jpg\" title=\"Cool brah\">"
-						+ "<img src=\"http://assets.blah.com/cool-img_thumbnail.jpg\" alt=\"Cool brah\"/></a>",
-				result);
-	}
-
-	@Test
-	public void imageWithQuotesInCaption() throws IOException {
-		context.getImage().setCaption("<a href=\"google.com\">a link</a>");
-
-		var result = renderTemplate("{{thumbnailImage image taxon}}", context);
-
-		assertEquals(
-				"<a href=\"http://assets.blah.com/cool-img_fullsize.jpg\" title=\"Cool brah\">"
-						+ "<img src=\"http://assets.blah.com/cool-img_thumbnail.jpg\" alt=\"Cool brah\"/></a>",
-				result);
-	}
-
-	@Test
-	public void imageWithQuotesInTitle() throws IOException {
-		context.getImage().setTitle("\"some title\"");
-
-		var result = renderTemplate("{{thumbnailImage image taxon}}", context);
-
-		assertEquals(
-				"<a href=\"http://assets.blah.com/cool-img_fullsize.jpg\" title=\"'some title'\">"
-						+ "<img src=\"http://assets.blah.com/cool-img_thumbnail.jpg\" alt=\"'some title'\"/></a>",
-				result);
+	public void testImageToThumbnailUrl() throws IOException {
+		var result = renderTemplate("{{imageToFullsizeUrl image}}", context);
+		assertEquals("http://assets.blah.com/cool-img_fullsize.jpg", result);
 	}
 }
