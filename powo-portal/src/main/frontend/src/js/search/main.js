dcd6b406dc597d07dd3d2cf534c4036af504edf2
Malcolm Stone <m.stone@kew.org>
2022-02-08 12:43:29 +0000

    use non-relative paths for search redirect

--- powo-portal/src/main/frontend/src/js/search/main.js
+++ powo-portal/src/main/frontend/src/js/search/main.js
@@ -72,13 +72,13 @@ define(function(require) {
       filters.initialize();
       filters.tokenfield().on("tokenfield:createtoken", function (e) {
         e.preventDefault();
-        window.location = "../results?q=" + e.attrs.value;
+        window.location = "/results?q=" + e.attrs.value;
       });
 
       $(document).on("click", "#search-button", function (e) {
         e.preventDefault();
         if ($(".token-input").val()) {
-          window.location = "../results?q=" + $(".token-input").val();
+          window.location = "/results?q=" + $(".token-input").val();
         }
       });
     },
