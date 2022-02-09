4d66c46eb2cf23f3adf3127fde8ebf0681159609
Malcolm Stone <m.stone@kew.org>
2022-02-09 14:55:11 +0000

    add back newlines to console libraries statement

--- powo-portal/src/main/frontend/src/js/main.js
+++ powo-portal/src/main/frontend/src/js/main.js
@@ -6,7 +6,7 @@ require.config({
 require(["search", "taxon", "static", "nav", "results"]);
 
 console.log(
-  "If you want to use POWO data programmatically, please check out our client libraries:" +
-    "\tPython\thttps://github.com/RBGKew/pykew" +
-    "\tR\t\thttps://github.com/barnabywalker/kewr"
+  "If you want to use POWO data programmatically, please check out our client libraries:\n" +
+    "\tPython https://github.com/RBGKew/pykew\n" +
+    "\tR https://github.com/barnabywalker/kewr"
 );
