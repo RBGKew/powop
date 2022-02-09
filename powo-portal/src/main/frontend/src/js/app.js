be16f1abdd10fe7961047c25ad2349918afbb25e
Malcolm Stone <m.stone@kew.org>
2022-02-09 14:45:20 +0000

    refactor app initialisation
    so that it is just slightly simpler - the redirection to app.js was just confusing, and the paths key was no longer required after removing the file
    also gets the results module importing properly

--- powo-portal/src/main/frontend/src/js/app.js
+++
@@ -1,8 +0,0 @@
-require([
-  "search", 
-  "taxon", 
-  "static", 
-  "nav"
-]);
-
-console.log("If you want to use POWO data programatically, please check out our python client https://github.com/RBGKew/pykew");
--- powo-portal/src/main/frontend/src/js/main.js
+++ powo-portal/src/main/frontend/src/js/main.js
@@ -1,18 +1,6 @@
-function version($log) {
-    window.console && console.log($log);
-}
-function dbg($log) {
-    window.console && console.log($log);
-}
-
 require.config({
-    baseUrl: '/src/js',
-
-    paths: {
-        app: 'app',
-    },
-
-    packages: ["search", "taxon", "static", "nav"]
+  baseUrl: "/src/js",
+  packages: ["search", "taxon", "static", "nav", "results"],
 });
 
-require(['app']);
+require(["search", "taxon", "static", "nav", "results"]);
