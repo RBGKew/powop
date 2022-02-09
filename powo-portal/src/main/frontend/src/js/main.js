1c420b6e26be6fac870270c5fcbca5c6d45a4878
Malcolm Stone <m.stone@kew.org>
2022-02-09 14:47:56 +0000

    add back console message about client libraries
    and add Baz's R library in!

--- powo-portal/src/main/frontend/src/js/main.js
+++ powo-portal/src/main/frontend/src/js/main.js
@@ -4,3 +4,9 @@ require.config({
 });
 
 require(["search", "taxon", "static", "nav", "results"]);
+
+console.log(
+  "If you want to use POWO data programmatically, please check out our client libraries:" +
+    "\tPython\thttps://github.com/RBGKew/pykew" +
+    "\tR\t\thttps://github.com/barnabywalker/kewr"
+);
